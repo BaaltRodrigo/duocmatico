736adb084e669e16d8e6450220a76232510550d7
BaaltRodrigo <r.antonio.regueiro@gmail.com>
2022-01-15 03:39:49 -0300

    refactor: Store basico para dejar los cursos en algun lugar comun

--- src/store/index.js
+++ src/store/index.js
@@ -3,9 +3,24 @@ import Vuex from "vuex";
 
 Vue.use(Vuex);
 
+const state = {
+  subioArchivo: false,
+  cursos: [],
+};
+
+const mutations = {
+  setCursos(state, cursos) {
+    state.cursos = cursos;
+  }
+};
+
+const actions = {};
+
+const modules = {};
+
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+  state,
+  mutations,
+  actions,
+  modules,
 });
