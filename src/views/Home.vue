ec7822394221c957b88d20592399b05b6436bca8
BaaltRodrigo <r.antonio.regueiro@gmail.com>
2022-01-15 01:51:29 -0300

    refactor: :fire: Quito todo y pongo una estructura que obviamente no sera la final

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,35 @@
 <template>
-  <hello-world />
+  <v-container>
+    <!-- Espacio para el calendario -->
+    <v-card class="mb-3" outlined>
+      <v-card-title>Aca va el calendario</v-card-title>
+      <v-card-text>Intentar mostrarlo por semana</v-card-text>
+    </v-card>
+    
+    <!-- Espacio para decir que suban el archivo -->
+    <v-card class="mb-3" outlined>
+      <v-card-title>Espacio para el input del archivo</v-card-title>
+      <v-card-text>
+        Aca va el input para subir el archivo. Solo se mostrara cuando no existen cursos cargados en la app
+      </v-card-text>
+    </v-card>
+
+    <v-card class="mb-3" outlined>
+      <v-card-title>Filtros del listado de cursos</v-card-title>
+      <v-card-text>Quizas termine quitando esto y poniendolo en otro lado</v-card-text>
+    </v-card>
+
+    <!-- Lugar para escoger los cursos -->
+    <v-card outlined>
+      <v-card-title>Listado con los cursos</v-card-title>
+      <v-card-text>Izi mockup</v-card-text>
+    </v-card>
+  </v-container>
 </template>
 
 <script>
-import HelloWorld from "../components/HelloWorld";
 
 export default {
   name: "Home",
-
-  components: {
-    HelloWorld,
-  },
 };
 </script>
