133b2030737dcbdff8004bf2ad7461fbbdacb5d5
BaaltRodrigo <r.antonio.regueiro@gmail.com>
2022-01-16 17:38:00 -0300

    refactor: :recycle: (MOMENTANEO) Simplifico lo que se ve en los cursos

---
+++ src/components/Curso.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-container>
+    <h4 class="text-capitalize">{{curso.asignatura.toLowerCase()}}</h4>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "Curso",
+
+  props: {
+    curso: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
