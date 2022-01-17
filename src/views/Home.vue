7ea4b5ec7b19ba330ac4e74028e2a155807ec295
BaaltRodrigo <r.antonio.regueiro@gmail.com>
2022-01-17 01:37:06 -0300

    refactor: Oculto cosas relacionadas con cursos cuando no existe carrera

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -18,25 +18,24 @@
     </v-card>
 
     <!-- Aca empiezan los cursos y las cosas para buscarlos -->
-    <h2 id="inicio-listado" class="text-center">Listado de cursos</h2>
-    <v-text-field
-      outlined
-      dense
-      prepend-inner-icon="mdi-magnify"
-      v-model="busqueda"
-      clearable
-      @click:clear="limpiarBusqueda"
-    ></v-text-field>
-    <!-- Listado de cursos -->
-    <v-card v-if="!!carrera" outlined>
-      <div
+    <div v-if="!!carrera">
+      <h2 id="inicio-listado" class="text-center">Listado de cursos</h2>
+      <v-text-field
+        outlined
+        dense
+        prepend-inner-icon="mdi-magnify"
+        v-model="busqueda"
+        clearable
+        @click:clear="limpiarBusqueda"
+      ></v-text-field>
+      <!-- Listado de cursos -->
+      <curso
         v-for="curso in filtroCursos"
         :key="curso.seccion"
-      >
-        <curso :curso="curso" />
-        <v-divider></v-divider>
-      </div>
-    </v-card>
+        :curso="curso"
+        class="mb-3 elevation-6"
+      />
+    </div>
   </v-container>
 </template>
 
@@ -59,7 +58,7 @@ export default {
     ...mapState(["carrera"]),
     
     filtroCursos() {
-      if (!this.carreraSeleccionada) return [];
+      if (!this.carrera) return [];
       if (!this.busqueda) return this.carrera.ramos;
       const buscar = this.busqueda.toUpperCase();
       return this.carrera.ramos.filter( r => 
