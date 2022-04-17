f8a0b9c4d305f0fb4e8fad2740262b3485fd889d
BaaltRodrigo <r.antonio.regueiro@gmail.com>
2022-04-17 01:53:25 -0400

    refactor: :recycle: Store provisional para la carga del archivo
    Una vez confirmada toda la carga, los datos pasan al store root

--- src/store/index.js
+++ src/store/index.js
@@ -1,31 +1,58 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import fileUpload from "./fileUpload.js";
 
 Vue.use(Vuex);
 
 const state = {
-  subioArchivo: false,
-  disponibles: [],
-  carrera: null,
+  careersData: [],
+  career: null,
 };
 
 const mutations = {
-  setDisponibles(state, disponibles) {
-    state.disponibles = disponibles;
+  setCareersData(state, data) {
+    state.careersData = data;
+    console.log(data);
   },
 
-  setCarrera(state, carrera) {
-    state.carrera = carrera;
+  setCareer(state, career) {
+    state.career = career;
   },
 };
 
-const actions = {};
+const getters = {
+  careerCourses(state) {
+    const index = state.careersData.findIndex(
+      (c) => c.carrera === state.career
+    );
+    console.log(index);
+    if (index < 0) return [];
+    return state.careersData[index].ramos;
+  },
 
-const modules = {};
+  careers(state) {
+    return [...new Set(state.careersData.map((data) => data.carrera))];
+  },
+};
+
+const actions = {
+  increaseStep(state) {
+    state.step++;
+  },
+
+  decreaseStep(state) {
+    state.setp--;
+  },
+};
+
+const modules = {
+  fileUpload,
+};
 
 export default new Vuex.Store({
   state,
   mutations,
+  getters,
   actions,
   modules,
 });
