132f35baa26353018abd73a4a638303b527e8367
BaaltRodrigo <r.antonio.regueiro@gmail.com>
2022-04-17 18:38:41 -0400

    refactor: :bulb: Comento el boton para agregar. Aun no se agregaran cursos a nada

--- src/components/CursoHeader.vue
+++ src/components/CursoHeader.vue
@@ -2,18 +2,18 @@
   <v-list-item two-line>
     <v-list-item-content>
       <v-list-item-subtitle>
-        <small>{{curso.seccion}}</small>
-        <v-chip class="ml-2" x-small>Semestre {{curso.nivel}}</v-chip>
+        <small>{{ curso.seccion }}</small>
+        <v-chip class="ml-2" x-small>Semestre {{ curso.nivel }}</v-chip>
       </v-list-item-subtitle>
       <v-list-item-title class="text-capitalize font-weight-bold">
-        {{curso.asignatura.toLowerCase()}}
+        {{ curso.asignatura.toLowerCase() }}
       </v-list-item-title>
     </v-list-item-content>
-    <v-list-item-action>
-      <v-btn icon outlined>
+    <!-- <v-list-item-action>
+      <v-btn icon outlined @click="test">
         <v-icon>mdi-plus</v-icon>
       </v-btn>
-    </v-list-item-action>
+    </v-list-item-action> -->
   </v-list-item>
 </template>
 
@@ -24,8 +24,14 @@ export default {
   props: {
     curso: {
       type: Object,
-      required: true
-    }
+      required: true,
+    },
   },
-}
+
+  methods: {
+    test() {
+      console.log(this.curso);
+    },
+  },
+};
 </script>
