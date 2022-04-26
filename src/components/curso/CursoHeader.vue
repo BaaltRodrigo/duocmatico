b3def665948e0756a1cdb888af905bf35f3110d4
BaaltRodrigo <r.antonio.regueiro@gmail.com>
2022-04-26 18:58:02 -0400

    refactor(Cursos): Dejo solamente el titulo de la seccion en CursoHeader

--- src/components/curso/CursoHeader.vue
+++ src/components/curso/CursoHeader.vue
@@ -1,12 +1,8 @@
 <template>
-  <v-list-item two-line>
+  <v-list-item>
     <v-list-item-content>
-      <v-list-item-subtitle>
-        <small>{{ curso.seccion }}</small>
-        <v-chip class="ml-2" x-small>Semestre {{ curso.nivel }}</v-chip>
-      </v-list-item-subtitle>
-      <v-list-item-title class="text-capitalize font-weight-bold">
-        {{ curso.asignatura.toLowerCase() }}
+      <v-list-item-title>
+        {{ curso.seccion }}
       </v-list-item-title>
     </v-list-item-content>
     <!-- <v-list-item-action>
