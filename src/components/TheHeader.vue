3dbb7ba808b35ffaf2df8586869a8033a61c6e54
BaaltRodrigo <r.antonio.regueiro@gmail.com>
2022-05-12 23:59:08 -0400

    refactor: :lipstick: Muevo header a un componente aparte

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-app-bar clipped-left app fixed dark color="purple" flat>
+    <v-app-bar-title>Duocmatico</v-app-bar-title>
+    <v-spacer></v-spacer>
+    <v-btn icon @click="wantUpload = true">
+      <v-icon>mdi-cloud-upload</v-icon>
+    </v-btn>
+    <v-btn icon href="https://github.com/BaaltRodrigo/duocmatico" target="none">
+      <v-icon>{{ "mdi-github" }}</v-icon>
+    </v-btn>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: "TheHeader",
+};
+</script>
