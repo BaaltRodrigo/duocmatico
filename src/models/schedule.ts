a022919cf5e69b101f259314634c23f8d8693ff5
BaaltRodrigo <r.antonio.regueiro@gmail.com>
2022-06-07 21:58:59 -0400

    refactor(Schedule): :sparkles: function to calculate equals to another schedule and isEmptySchedule

--- src/models/schedule.ts
+++ src/models/schedule.ts
@@ -1,22 +1,48 @@
 /**
  * Class used to represent the block of time of a real class.
  *
- * @param {String} start time string with the format 'HH:MM:SS'
- * @param {String} end time string with the format 'HH:MM:SS'
+ * @param {String} baseString schedule time given by xslx
  * @param {String} day WeekDay for this timetable
  * @param {String} classroom Where we do this timetable.
  */
 class Schedule {
-  start: string;
-  end: string;
+  EMPTY_SCHEDULE: string = "0:00:00 - 0:00:00";
+  baseString: string;
+  start: string | null = null;
+  end: string | null = null;
   day: string;
   classroom: string;
 
-  constructor(start: string, end: string, day: string, classroom: string) {
-    this.start = start;
-    this.end = end;
+  constructor(baseString: string, day: string, classroom: string) {
+    this.baseString = baseString;
     this.day = day;
     this.classroom = classroom;
+    this._calculateTimeBlock();
+  }
+
+  isEmptySchedule(): Boolean {
+    return this.baseString == this.EMPTY_SCHEDULE;
+  }
+
+  equals(other: Schedule) {
+    return (
+      this.start === other.start &&
+      this.end === other.end &&
+      this.day === other.day
+    );
+  }
+
+  /**
+   * Calculate start and end of the time block;
+   *
+   * @returns {void}
+   */
+  _calculateTimeBlock(): void {
+    if (this.isEmptySchedule()) return; // Keep null values
+    const timesArray = this.baseString.split(" - ");
+    this.end = timesArray[1];
+    this.start = timesArray[0].slice(3);
+    return;
   }
 }
 
