d5efe7da578c396732d796e0066d612faf6cbd94
BaaltRodrigo <r.antonio.regueiro@gmail.com>
2022-06-09 14:42:32 -0400

    refactor(fileConverter): :recycle: Add ExcelFileRow interface and function to transform ExcelRows to Section class

---
+++ src/helpers/fileConverter.ts
@@ -0,0 +1,149 @@
+import Schedule from "@/models/schedule";
+import Section from "@/models/section";
+import readXlsxFile from "read-excel-file";
+
+export interface ExcelFileRow {
+  abbreviation: string;
+  campus: string;
+  career: string;
+  classroom: string;
+  day: string;
+  daytime: string;
+  level: number;
+  period: number;
+  plan: number;
+  scheduleString: string;
+  school: string;
+  section: string;
+  subjectType: string;
+  subject: string;
+  teacher: string;
+  year: number;
+}
+
+/**
+ * Used to convert every row of xlsx into a json object
+ * with the headers as keys
+ *
+ * @param {File} file
+ * @returns
+ */
+function mapFileContent(file: File) {
+  // Headers we want to keep from file
+  const map = {
+    Año: "year",
+    Período: "period",
+    Sede: "campus",
+    Escuela: "school",
+    Carrera: "career",
+    Plan: "plan",
+    "Tipo Plan de Estudios": "typePlan",
+    Jornada: "daytime",
+    Nivel: "level",
+    "Tipo Asignatura": "typeSubject",
+    Sigla: "abbreviation",
+    Asignatura: "subject",
+    Sección: "section",
+    Horario: "scheduleString",
+    Sala: "classroom",
+    Docente: "teacher",
+    Día: "day",
+  };
+  return readXlsxFile(file, { map });
+}
+
+/**
+ * Used to convert data inside xlsx to an array of Sections
+ *
+ * @param {object[]} rows All rows from xlsx file provided by DuocUC
+ * @returns {Section[]} Array with mapped information
+ */
+function excelRowsToSections(rows: ExcelFileRow[]): Section[] {
+  const sections: Section[] = [];
+  // Read line
+  rows.forEach((row: ExcelFileRow) => {
+    // Get information to create Schedule class
+    const { day, scheduleString, classroom } = row; // Schedule information
+    const schedule = new Schedule(scheduleString, day, classroom);
+    // If Section does not exist, create it. else, find it
+    const sectionIndex = sections.findIndex(
+      (s: Section) => s.section === row.section
+    );
+    const section =
+      sectionIndex >= 0 ? sections[sectionIndex] : new Section(row);
+    // Add schedule to that section
+    section.addSchedule(schedule);
+    section.addPlan(row.plan);
+
+    if (sectionIndex === -1) sections.push(section);
+  });
+
+  return sections;
+}
+
+function groupBySections(rows: object[]) {
+  const uniqueNames = [...new Set(rows.map((row) => row.seccion))];
+  const sections = uniqueNames.map((sectionName) => {
+    const sameSection = rows.filter((row) => row.seccion === sectionName);
+    const horarios = sameSection.map(({ horario, dia, sala }) => {
+      return { horario, dia, sala };
+    });
+    const planes = [...new Set(sameSection.map((s) => s.plan))];
+    const first = sameSection[0];
+    // Clean unnecesary data
+    delete first.plan;
+    delete first.horario;
+    delete first.dia;
+    delete first.sala;
+    return {
+      ...first,
+      ...{ horarios: uniqueSchedules(horarios) },
+      planes,
+      nivel: first.nivel || null,
+    };
+  });
+  return sections;
+}
+
+function groupByCareer(sections) {
+  const uniqueNames = [...new Set(sections.map((s) => s.carrera))];
+  const careers = uniqueNames.map((career) => {
+    let sameCareer = sections.filter((s) => s.carrera === career);
+    sameCareer = sameCareer.map((c) => {
+      delete c.carrera;
+      return c;
+    });
+    return {
+      carrera: career,
+      ramos: sameCareer,
+    };
+  });
+  // This sort method does nothing on some browsers
+  careers.sort((a, b) => (a.carrera > b.carrera ? 1 : 0));
+  return careers;
+}
+
+/**
+ * Used to make sections schedules uniques.
+ * Sometimes there are sections with duplicated schedules. This make them uniques
+ *
+ * @param {Array} horarios
+ * @return {Array} Array with unique Schedules
+ */
+function uniqueSchedules(horarios) {
+  const noDuplicates = [...new Set(horarios.map((h) => h.horario))];
+  const schedules = noDuplicates.map((hours) => {
+    // Can do this because hours comes with the same array.
+    // There is 100% chance that I will find AT LEAST one match
+    return horarios.filter((h) => h.horario === hours)[0];
+  });
+  return schedules;
+}
+
+export {
+  mapFileContent,
+  groupBySections,
+  groupByCareer,
+  uniqueSchedules,
+  excelRowsToSections,
+};
