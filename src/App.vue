07808fea1afbcd222c29fe0877fe6435ca4ccbc9
BaaltRodrigo <r.antonio.regueiro@gmail.com>
2022-06-27 21:47:09 -0400

    refactor: :recycle: Migrate App.vue to TypeScript

--- src/App.vue
+++ src/App.vue
@@ -8,11 +8,13 @@
 </template>
 
 <script>
-import { mapActions, mapMutations, mapState } from "vuex";
+import Vue from "vue";
+import { mapMutations, mapState } from "vuex";
 import { version } from "../package";
 import TheHeader from "./components/TheHeader.vue";
+import Section from "./models/section";
 
-export default {
+export default Vue.extend({
   name: "App",
 
   components: {
@@ -24,48 +26,53 @@ export default {
   }),
 
   computed: {
-    ...mapState(["careersData"]),
+    ...mapState(["xlsxRows"]),
   },
 
   methods: {
-    ...mapMutations(["setCareersData"]),
-    ...mapActions(["findCareerData"]),
-    ...mapMutations("courses", ["setCareer", "setCourses"]),
-    ...mapMutations("schedule", ["setSections"]),
+    ...mapMutations("sections", ["setSections", "setCareer"]),
+
+    loadExcelData() {
+      if (!localStorage.excelRows) return;
+      const excelRows = JSON.parse(localStorage.excelRows);
+      const sections = Section.mapFromExcelRows(excelRows);
+
+      this.setSections(sections);
+      console.log("Load sections:", sections);
+    },
 
     loadSavedSchedule() {
       const schedule = localStorage.schedule;
-      if (!schedule) return;
 
-      this.setSections(JSON.parse(schedule));
+      const scheduledSections = Section.mapFromExcelRows(JSON.parse(schedule));
+      console.log("Saved schedule:", scheduledSections);
+      this.$store.commit("schedule/setSections", scheduledSections);
     },
   },
 
   async mounted() {
     // Check LocalStorage for xlsxJsonData
-    const { xslxJsonData, selectedCareer } = localStorage;
-    if (xslxJsonData && selectedCareer) {
-      this.setCareersData(JSON.parse(xslxJsonData));
-      const careerData = await this.findCareerData(selectedCareer);
-      this.setCareer(selectedCareer);
-      this.setCourses(careerData.ramos);
-    }
   },
 
   created() {
     // Clean the localStorage every time there is a major update
     // By every time I mean, every time we do a change on how the
-    // xslx file is readed.
-    if (!(version == localStorage.lastVersion)) {
+    // xslx file is read.
+    const subVersions = version.split(".");
+    if (subVersions[0] != "2") {
       localStorage.clear();
     }
+
+    // We can do this without any data because it will resolve into null or undefined
+    const { selectedCareer, excelRows } = localStorage;
+    this.setCareer(selectedCareer);
+
+    const sections = Section.mapFromExcelRows(JSON.parse(excelRows));
+    this.setSections(sections);
+
+    console.log("Loaded Sections:", sections);
+
     this.loadSavedSchedule();
   },
-};
+});
 </script>
-
-<style scoped>
-html {
-  overflow-y: auto;
-}
-</style>
