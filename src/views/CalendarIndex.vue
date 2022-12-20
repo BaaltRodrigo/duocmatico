2e0d0fc275d28eb63d7f743c1f089846608e74db
BaaltRodrigo <r.antonio.regueiro@gmail.com>
2022-12-20 01:14:11 -0300

    feat(Calendarios): :construction: Wireframe interfaz para visualizar el indice de calendarios

---
+++ src/views/CalendarIndex.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container>
+    <h3>Calendarios disponibles</h3>
+    <v-row>
+      <v-col
+        v-for="i in [1, 2, 3, 4, 5]"
+        :key="`calendar-${i}`"
+        cols="12"
+        md="4"
+      >
+        <v-sheet height="15vh" color="grey" rounded="lg"></v-sheet>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script></script>
