912f9661e05a65ce413ce46a3ca5e91037bc5b5d
BaaltRodrigo <r.antonio.regueiro@gmail.com>
2022-12-26 17:31:55 -0300

    refactor(Calendarios/DmCalendarCard): :recycle: Cambio de visual y agrego eventos para eliminar y mostrar

--- src/components/calendar/DmCalendarCard.vue
+++ src/components/calendar/DmCalendarCard.vue
@@ -1,9 +1,36 @@
 <template>
-  <v-card height="15vh" rounded="lg">
-    <v-card-title>{{ calendar.name }}</v-card-title>
-    <v-card-subtitle>
-      {{ calendar.carga }} - {{ calendar.carrera }}
-    </v-card-subtitle>
+  <v-card @click="$emit('showCalendar')" height="16vh" rounded="lg">
+    <v-list>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title v-text="calendar.name"></v-list-item-title>
+        </v-list-item-content>
+        <v-list-item-action>
+          <v-btn icon @click.stop="$emit('deleteMe')">
+            <v-icon>mdi-delete</v-icon>
+          </v-btn>
+        </v-list-item-action>
+      </v-list-item>
+    </v-list>
+    <v-simple-table dense>
+      <template v-slot:default>
+        <thead></thead>
+        <tbody>
+          <tr>
+            <th>Carrera</th>
+            <td class="text-capitalize text-right">
+              {{ calendar.carrera.toLowerCase() }}
+            </td>
+          </tr>
+          <tr>
+            <th>Ramos</th>
+            <td class="text-right">
+              {{ calendar.sections.length }}
+            </td>
+          </tr>
+        </tbody>
+      </template>
+    </v-simple-table>
   </v-card>
 </template>
 
