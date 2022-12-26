9e6e0cabe826b496729b98ab667621cf3a42d772
BaaltRodrigo <r.antonio.regueiro@gmail.com>
2022-12-25 22:23:08 -0300

    feat(Calendarios/DmCalendarCard): :construction: Agrego carta para mostrar los calendarios en el indice

---
+++ src/components/calendar/DmCalendarCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-card height="15vh" rounded="lg">
+    <v-card-title>{{ calendar.name }}</v-card-title>
+    <v-card-subtitle>
+      {{ calendar.carga }} - {{ calendar.carrera }}
+    </v-card-subtitle>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "DmCalendarCard",
+
+  props: {
+    calendar: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
