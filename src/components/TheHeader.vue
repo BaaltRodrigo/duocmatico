023d79ebbd62d7ccc11a8f5a9a5ccac92ad7d916
BaaltRodrigo <r.antonio.regueiro@gmail.com>
2022-12-26 17:34:55 -0300

    refactor: :recycle: Simplifico el header

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,22 +1,28 @@
 <template>
-  <v-app-bar clipped-left app fixed dark color="purple" flat>
-    <v-app-bar-title>Duocmatico</v-app-bar-title>
-    <v-spacer></v-spacer>
-    <v-btn icon @click="setShowModal(true)">
-      <v-icon>mdi-cloud-upload</v-icon>
-    </v-btn>
-    <v-btn icon href="https://github.com/BaaltRodrigo/duocmatico" target="none">
-      <v-icon>{{ "mdi-github" }}</v-icon>
-    </v-btn>
+  <v-app-bar app fixed dark color="purple" flat>
+    <v-container class="py-0 fill-height">
+      <v-app-bar-title>Duocmatico</v-app-bar-title>
+      <v-btn text @click="$router.push({ name: 'calendar.index' })">
+        Mis Calendarios
+      </v-btn>
+      <v-spacer></v-spacer>
+      <v-btn
+        icon
+        href="https://github.com/BaaltRodrigo/duocmatico"
+        target="none"
+      >
+        <v-icon>{{ "mdi-github" }}</v-icon>
+      </v-btn>
+    </v-container>
   </v-app-bar>
 </template>
 
 <script>
-import { mapMutations } from "vuex";
 export default {
   name: "TheHeader",
-  methods: {
-    ...mapMutations("fileUpload", ["setShowModal"]),
-  },
+
+  data: () => ({
+    buttonOptions: [{ title: "Mis calendarios", page: "calendar.index" }],
+  }),
 };
 </script>
