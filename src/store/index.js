66d0dbc2f96e96931ae4865a5a3bd19d3258df0b
BaaltRodrigo <r.antonio.regueiro@gmail.com>
2023-01-05 03:27:24 -0300

    refactor: :fire: Elimino codigo sin uso del store principal

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,6 @@
 import Vue from "vue";
 import Vuex from "vuex";
 import fileUpload from "./fileUpload.js";
-// const courses = require("./courses.js");
-import courses from "./courses.js";
 import calendars from "./calendars.js";
 import schedule from "./schedule.js";
 import firebase from "./firebase.js";
@@ -10,15 +8,10 @@ import firebase from "./firebase.js";
 Vue.use(Vuex);
 
 const state = {
-  careersData: [],
   eventLog: [],
 };
 
 const mutations = {
-  setCareersData(state, data) {
-    state.careersData = data;
-  },
-
   addLogEvent(state, event) {
     const eventDate = new Date();
     state.eventLog.push(`[${eventDate.toISOString()}] ${event}`);
@@ -26,50 +19,8 @@ const mutations = {
   },
 };
 
-const getters = {
-  careerCourses(state) {
-    const index = state.careersData.findIndex(
-      (c) => c.carrera === state.career
-    );
-    console.log(index);
-    if (index < 0) return [];
-    return state.careersData[index].ramos;
-  },
-
-  careers(state) {
-    return [...new Set(state.careersData.map((data) => data.carrera))];
-  },
-
-  semesters(state, getters) {
-    let allSemesters = getters.careerCourses.map(
-      (course) => course.nivel || null
-    );
-    return [...new Set(allSemesters)].sort((a, b) => a > b);
-  },
-
-  times(state, getters) {
-    return [...new Set(getters.careerCourses.map((c) => c.jornada))];
-  },
-};
-
-const actions = {
-  increaseStep(state) {
-    state.step++;
-  },
-
-  decreaseStep(state) {
-    state.setp--;
-  },
-
-  findCareerData({ state }, career) {
-    let index = state.careersData.findIndex((c) => c.carrera === career);
-    return state.careersData[index] || [];
-  },
-};
-
 const modules = {
   calendars,
-  courses,
   firebase,
   fileUpload,
   schedule,
@@ -78,7 +29,5 @@ const modules = {
 export default new Vuex.Store({
   state,
   mutations,
-  getters,
-  actions,
   modules,
 });
