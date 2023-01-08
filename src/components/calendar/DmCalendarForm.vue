13af4ce92b7b48b75c74173bf0b6e315ca1de0d6
BaaltRodrigo <r.antonio.regueiro@gmail.com>
2023-01-08 02:51:24 -0300

    refactor(Calendarios/DmCalendarForm): :recycle: Form emite `created` cuando un calendario nuevo es creado
    Ademas se traen las cargas acadmicas disponibles cuando se monta el form

--- src/components/calendar/DmCalendarForm.vue
+++ src/components/calendar/DmCalendarForm.vue
@@ -12,7 +12,7 @@
           label="Carga academica"
           :items="cargasAcademicas"
           @input="changeCargaAcademica"
-          v-model="fileForm"
+          v-model="cargaForm"
         ></v-autocomplete>
         <v-autocomplete
           label="Carrera"
@@ -46,24 +46,28 @@ export default {
     ...mapState("firebase", ["cargasAcademicas", "carga", "carreras"]),
 
     isDisabled() {
-      return !(this.name && this.fileForm && this.careerForm);
+      return !(this.name && this.cargaForm && this.careerForm);
     },
   },
 
   data: () => ({
     name: null,
-    fileForm: null,
+    cargaForm: null,
     careerForm: null,
   }),
 
   methods: {
-    ...mapActions("firebase", ["setCargaAcademica", "setCarrera"]),
+    ...mapActions("firebase", [
+      "setCargaAcademica",
+      "getCargasFromFirebase",
+      "setCarrera",
+    ]),
     ...mapMutations("calendars", ["addCalendar"]),
 
     async changeCargaAcademica() {
       this.careerForm = null; // Esto ocurre siempre que cambia la carga academica
-      if (!this.fileForm) return; // Carga academica es borrada -> no hago nada
-      await this.setCargaAcademica(this.fileForm);
+      if (!this.cargaForm) return; // Carga academica es borrada -> no hago nada
+      await this.setCargaAcademica(this.cargaForm);
     },
 
     async changeCarrera() {
@@ -73,11 +77,16 @@ export default {
     createCalendar() {
       this.addCalendar({
         name: this.name,
-        carga: this.fileForm,
+        carga: this.cargaForm,
         carrera: this.careerForm,
         sections: [],
       });
+      this.$emit("created");
     },
+  },
+
+  async mounted() {
+    await this.getCargasFromFirebase();
   },
 };
 </script>
