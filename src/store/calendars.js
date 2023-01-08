ccf6f877b0e8420e1b368bd389ce9fe5131398e1
BaaltRodrigo <r.antonio.regueiro@gmail.com>
2023-01-08 02:54:04 -0300

    feat(Calendarios/store): :sparkles: Acciones para setear calendario seleccionado, borrar y agregar
    Estas acciones son mas que nada un compilado de commits, para poder trackear y setear toda la info neccesaria

--- src/store/calendars.js
+++ src/store/calendars.js
@@ -13,6 +13,30 @@ const state = {
   selected: null,
 };
 
+const getters = {
+  /**
+   * Used to map all levels of the sections and return an array whit
+   * unique levels.
+   *
+   * @returns {array} Non repetitive levels
+   */
+  getSemesters({ selected }) {
+    const { sections } = selected;
+    let allSemesters = sections.map((c) => c.nivel || null);
+    return [...new Set(allSemesters)].sort((a, b) => a > b);
+  },
+
+  /**
+   * Gives an array whit the day shifts for the sections available
+   *
+   * @returns {array} day shifts
+   */
+  getTimes({ selected }) {
+    const { sections } = selected;
+    return [...new Set(sections.map((c) => c.jornada))];
+  },
+};
+
 const mutations = {
   setCalendars(state, calendars) {
     state.all = calendars;
@@ -20,7 +44,6 @@ const mutations = {
 
   addCalendar(state, calendar) {
     state.all.push(calendar);
-    localStorage.calendars = JSON.stringify(state.all);
   },
 
   setSelected(state, index) {
@@ -29,7 +52,6 @@ const mutations = {
 
   deleteCalendar(state, index) {
     state.all.splice(index, 1);
-    localStorage.calendars = JSON.stringify(state.all);
   },
 
   addSection(state, section) {
@@ -46,22 +68,77 @@ const mutations = {
 };
 
 const actions = {
-  loadCalendarsFromLocalStorage({ commit }) {
+  addCalendarAction({ state, commit }, calendar) {
+    commit("addCalendar", calendar);
+    commit("addLogEvent", `Created calendar ${calendar.nombre}`, {
+      root: true,
+    });
+    localStorage.calendars = JSON.stringify(state.all);
+    commit("addLogEvent", `Updated localStorage calendars`, {
+      root: true,
+    });
+  },
+
+  deleteCalendarAction({ state, commit }, index) {
+    if (state.all.length < index || index < 0) {
+      commit("addLogEvent", `Calendar index to delete out of range`, {
+        root: true,
+      });
+      return;
+    }
+
+    const { name } = state.all[index];
+    commit("deleteCalendar", index);
+    commit("addLogEvent", `Deleted calendar ${name}`, { root: true });
+    localStorage.calendars = JSON.stringify(state.all);
+    commit("addLogEvent", `Updated localStorage calendars`, {
+      root: true,
+    });
+  },
+
+  loadCalendarsFromLocalStorage({ state, commit }) {
     const { calendars } = localStorage;
     if (calendars) {
       commit("setCalendars", JSON.parse(calendars));
+      commit("addLogEvent", `Loaded ${state.all.length} saved calendars`, {
+        root: true,
+      });
+    } else {
+      commit("addLogEvent", "No calendars to load", { root: true });
     }
+  },
+
+  saveCalendarsToLocalStorage({ state }) {
+    const { calendars } = state;
+    const calendarsSimplified = calendars.map((calendar) => {
+      let sections = calendar.sections.map((section) => section.seccion);
+      return {
+        ...calendar,
+        sections: sections,
+      };
+    });
+    // localStorage.calendars = calendarsSimplified;
+    console.log(calendarsSimplified);
   },
 
   setCalendarByIndex({ state, commit }, index) {
     if (index > state.all.length) return;
     commit("setSelected", index);
+    commit("addLogEvent", `Selected calendar number: ${index}`, { root: true });
+    const { carrera, carga } = state.selected;
+    // Change the state of firebase module in case user wants to add sections to their calendars
+    commit("firebase/setCarga", carga, { root: true });
+    commit("addLogEvent", `Carga set to: ${carga}`, { root: true });
+
+    commit("firebase/setCarrera", carrera, { root: true });
+    commit("addLogEvent", `Carrera set to: ${carrera}`, { root: true });
   },
 };
 
 export default {
   namespaced: true,
   state,
+  getters,
   mutations,
   actions,
 };
