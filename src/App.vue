5e44492e65139166e4ea5eb32e1941d50cde4c9f
BaaltRodrigo <r.antonio.regueiro@gmail.com>
2023-01-18 00:05:17 -0300

    refactor: :fire: Clean code on App.vue

--- src/App.vue
+++ src/App.vue
@@ -9,10 +9,7 @@
 
 <script>
 import { mapActions, mapMutations } from "vuex";
-// import { version } from "../package";
 import TheHeader from "./components/TheHeader.vue";
-
-// import cargaAcademica from "../public/cargas-academicas/2023-1 SAN-JOAQUIN.json";
 
 export default {
   name: "App",
@@ -30,31 +27,11 @@ export default {
     ...mapActions(["findCareerData"]),
     ...mapActions("calendars", ["loadCalendarsFromLocalStorage"]),
     ...mapActions("firebase", ["uploadCargaAcademica2"]),
-    ...mapMutations("schedule", ["setSections"]),
-
-    loadSavedSchedule() {
-      const schedule = localStorage.schedule;
-      if (!schedule) return;
-
-      this.setSections(JSON.parse(schedule));
-    },
-  },
-
-  async mounted() {
-    // Check LocalStorage for xlsxJsonData
-    // console.log(cargaAcademica);
-    // await this.uploadCargaAcademica2(cargaAcademica);
   },
 
   created() {
     this.addLogEvent("Creating application");
-    // Clean the localStorage every time there is a major update
-    // By every time I mean, every time we do a change on how the
-    // xslx file is readed.
-    // if (!(version == localStorage.lastVersion)) {
-    //   localStorage.clear();
-    // }
-    // this.loadSavedSchedule();
+
     this.loadCalendarsFromLocalStorage();
   },
 };
