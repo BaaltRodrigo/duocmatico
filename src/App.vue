306c9d4ca9b7bed72106d8e1451dad6e7f903a87
BaaltRodrigo <r.antonio.regueiro@gmail.com>
2023-01-17 21:59:07 -0300

    Dejo de limpiar el local storage.
    Al usar un nuevo dominio, no tenemos ultima version guardada. Ergo: el localstorage siempre se limpiaba

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@
 
 <script>
 import { mapActions, mapMutations } from "vuex";
-import { version } from "../package";
+// import { version } from "../package";
 import TheHeader from "./components/TheHeader.vue";
 
 // import cargaAcademica from "../public/cargas-academicas/2023-1 SAN-JOAQUIN.json";
@@ -51,9 +51,9 @@ export default {
     // Clean the localStorage every time there is a major update
     // By every time I mean, every time we do a change on how the
     // xslx file is readed.
-    if (!(version == localStorage.lastVersion)) {
-      localStorage.clear();
-    }
+    // if (!(version == localStorage.lastVersion)) {
+    //   localStorage.clear();
+    // }
     // this.loadSavedSchedule();
     this.loadCalendarsFromLocalStorage();
   },
