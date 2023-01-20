a0c6126bc9890a09a3ab3b1f9fecb2b3e8a557ac
BaaltRodrigo <r.antonio.regueiro@gmail.com>
2023-01-20 14:49:46 -0300

    feat(errors): :children_crossing: 404 component can redirect to calendar index

--- src/views/DmError404.vue
+++ src/views/DmError404.vue
@@ -3,10 +3,10 @@
     <v-card rounded="lg" outlined>
       <v-container>
         <v-row>
-          <v-col cols="12" md="6">
+          <v-col cols="12" sm="6">
             <v-img src="../../public/images/undraw_page_not_found.svg"></v-img>
           </v-col>
-          <v-col cols="12" md="6">
+          <v-col cols="12" sm="6">
             <v-card-title class="text-center text-h2">
               Nada por aqui!
             </v-card-title>
@@ -20,7 +20,11 @@
               Lo bueno, es que podemos ofrecerte varias cosas ;)
             </v-card-text>
             <v-card-actions>
-              <v-btn class="teal lighten-4">Ir a tus calendarios</v-btn>
+              <v-btn
+                class="teal lighten-4"
+                @click="$router.push({ name: 'calendar.index' })"
+                >Ir a tus calendarios</v-btn
+              >
             </v-card-actions>
           </v-col>
         </v-row>
