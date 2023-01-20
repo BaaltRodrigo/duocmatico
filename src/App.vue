c1f027cda9a4557ff255c12d7fffc8ac280e7d0e
BaaltRodrigo <r.antonio.regueiro@gmail.com>
2023-01-20 14:51:50 -0300

    refactor: :recycle: App can display 404 on every page without change route path

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,27 @@
   <v-app>
     <the-header></the-header>
     <v-main class="background">
-      <router-view />
+      <dm-error404 v-if="display404"></dm-error404>
+      <router-view v-else />
     </v-main>
   </v-app>
 </template>
 
 <script>
-import { mapActions, mapMutations } from "vuex";
+import { mapActions, mapMutations, mapState } from "vuex";
+import DmError404 from "./views/DmError404.vue";
 import TheHeader from "./components/TheHeader.vue";
 
 export default {
   name: "App",
 
   components: {
+    DmError404,
     TheHeader,
+  },
+
+  computed: {
+    ...mapState(["display404"]),
   },
 
   data: () => ({
