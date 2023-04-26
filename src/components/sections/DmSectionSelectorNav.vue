8adf73e9be1092c109344c5e26a5046258241c2a
BaaltRodrigo <r.antonio.regueiro@gmail.com>
2023-04-26 01:05:40 -0400

    feat(sections): :construction: sidebar works as duocmatico1
    Also provide filtering text input

--- src/components/sections/DmSectionSelectorNav.vue
+++ src/components/sections/DmSectionSelectorNav.vue
@@ -1,39 +1,91 @@
 <template>
   <v-navigation-drawer
-    class="pl-4"
+    app
     floating
-    width="480"
+    width="500"
+    class="pa-4 mb-4"
     :model-value="sectionsSidebar"
+    transition="slide-y-reverse transition-fast"
   >
-    <v-card
-      height="90%"
-      class="rounded-xl mt-2 mb-4 bg-white elevation-0"
-      variant="outlined"
-    >
-      <v-card-title class="text-h5 font-weight-regular">Secciones</v-card-title>
-      <!-- For some reason, having a card to overflow inside prevents the scrolls to render OUTSIDE the card -->
-      <v-card variant="flat" class="overflow-auto" height="90%">
-        <v-list lines="two">
-          <v-list-item
-            v-for="section in sectionsGroupedByCourse"
-            :key="section.name"
-            @click="() => {}"
-            :title="section.name"
-            :subtitle="`Secciones disponibles: ${section.sections.length}`"
-            class="text-capitalize"
+    <template v-slot:prepend>
+      <v-chip
+        size="x-large"
+        style="width: 100%"
+        variant="outlined"
+        class="bg-white"
+      >
+        <v-text-field
+          variant="plain"
+          placeholder="Buscar sección"
+          v-model="search"
+        ></v-text-field>
+      </v-chip>
+    </template>
+
+    <template v-slot:append>
+      <v-btn
+        block
+        variant="outlined"
+        class="rounded-pill"
+        @click="toggleSectionsSidebar"
+      >
+        Finalizar
+      </v-btn>
+    </template>
+
+    <v-card height="95%" class="my-4 rounded-xl bg-white" variant="outlined">
+      <v-breadcrumbs density="compact">
+        <v-breadcrumbs-item>
+          <v-btn
+            @click="step = 1"
+            variant="text"
+            class="rounded-pill text-capitalize text-body-2"
           >
-          </v-list-item>
-        </v-list>
-      </v-card>
+            Secciones
+          </v-btn>
+        </v-breadcrumbs-item>
+        <div v-if="step == 2">
+          <v-breadcrumbs-divider></v-breadcrumbs-divider>
+          <v-breadcrumbs-item>
+            <v-btn
+              variant="text"
+              class="rounded-pill text-body-2 text-capitalize"
+            >
+              {{ group.name.toLowerCase() }}
+            </v-btn>
+          </v-breadcrumbs-item>
+        </div>
+      </v-breadcrumbs>
+      <v-divider></v-divider>
+      <!-- Needs to set styles to force scrollbar -->
+      <v-window v-model="step" style="height: 90%; overflow: auto">
+        <!-- window to select course -->
+        <v-window-item :value="1">
+          <v-list two-line>
+            <v-list-item
+              v-for="(course, index) in filteredSectionGroups"
+              :key="course.name"
+              @click="selectCourse(course)"
+            >
+              <v-list-item-title class="text-capitalize">
+                {{ course.name.toLowerCase() }}
+              </v-list-item-title>
+              <v-list-item-subtitle>
+                Secciones disponibles: {{ course.sections.length }}
+              </v-list-item-subtitle>
+              <template #append>
+                <v-icon>mdi-chevron-down</v-icon>
+              </template>
+            </v-list-item>
+          </v-list>
+        </v-window-item>
+        <!-- Window to select sections -->
+        <v-window-item :value="2" class="px-8">
+          <!-- The group selected name -->
+          Poner cosas aca
+        </v-window-item>
+      </v-window>
     </v-card>
-    <v-btn
-      block
-      class="rounded-xl"
-      variant="outlined"
-      @click="toggleSectionsSidebar"
-    >
-      Finalizar
-    </v-btn>
   </v-navigation-drawer>
 </template>
 
@@ -46,19 +98,41 @@ export default {
   computed: {
     ...mapState("calendars", ["sectionsSidebar"]),
     ...mapGetters("academicCharges", ["sectionsGroupedByCourse"]),
+
+    // Filter sectionsGroupedByCourse name and initials by search
+    filteredSectionGroups() {
+      if (!this.search) return this.sectionsGroupedByCourse;
+
+      return this.sectionsGroupedByCourse.filter((course) => {
+        return (
+          course.name.toLowerCase().includes(this.search.toLowerCase()) ||
+          course.initials.toLowerCase().includes(this.search.toLowerCase())
+        );
+      });
+    },
   },
 
   data: () => ({
     step: 1,
+    group: {},
+    panels: [],
+    search: "",
   }),
 
   methods: {
     ...mapActions("calendars", ["toggleSectionsSidebar"]),
-  },
 
-  mounted() {
-    console.log(this.sectionsSidebar);
-    console.log(this.sectionsGroupedByCourse);
+    selectCourse(groupSelected) {
+      this.step = 2;
+      this.group = groupSelected;
+      console.log(this.group);
+    },
   },
 };
 </script>
+
+<style>
+.custom-input {
+  border-radius: 20px;
+}
+</style>
