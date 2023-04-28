d6f582f2f719066eae0f2e4488fb6280ee9d918c
BaaltRodrigo <r.antonio.regueiro@gmail.com>
2023-04-28 00:58:41 -0400

    feat(calendars): :sparkles: Show section events (schedules) inside a calendar
    By default we put a default color `purple`. May change it later

--- src/views/CalendarShow.vue
+++ src/views/CalendarShow.vue
@@ -11,7 +11,27 @@
         :time-from="8 * 60"
         :time-step="30"
         locale="es"
-      />
+        :events="calendarEvents"
+      >
+        <template v-slot:event="{ event }">
+          <v-card
+            class="rounded-lg"
+            variant="outlined"
+            height="100%"
+            :color="event.color"
+          >
+            <v-card-title class="text-capitalize text-body-2">{{
+              event.title
+            }}</v-card-title>
+            <v-card-text>
+              {{ event.start.toTimeString().slice(0, 5) }} -
+              {{ event.end.toTimeString().slice(0, 5) }}
+              <br />
+              Sala: {{ event.sala }}
+            </v-card-text>
+          </v-card>
+        </template>
+      </vue-cal>
     </v-card>
     <br />
     <v-btn
@@ -41,6 +61,10 @@ export default {
     ...mapState("calendars", ["selectedCalendar"]),
     ...mapState("academicCharges", ["secciones"]),
     ...mapGetters("academicCharges", ["sectionsGroupedByCourse"]),
+
+    calendarEvents() {
+      return this.getCalendarEvents();
+    },
   },
 
   methods: {
@@ -54,6 +78,70 @@ export default {
       }
       this.$store.dispatch("calendars/toggleSectionsSidebar");
     },
+
+    getCalendarEvents() {
+      const { sections } = this.selectedCalendar;
+      return sections.map((section) => this.getSectionEvents(section)).flat();
+    },
+
+    getSectionEvents(section) {
+      const { horarios } = section;
+      return horarios
+        .filter((h) => h.horario != "0:00:00 - 0:00:00") // Deleting non valid schedules
+        .map((schedule) => {
+          // const { start, end } = this.getScheduleCalendarEvents(schedule);
+          // For some reason, there is no need to substract the timezone offset
+          // start.setHours(start.getHours() - start.getTimezoneOffset() / 60);
+          // end.setHours(end.getHours() - end.getTimezoneOffset() / 60);
+          return {
+            title: section.asignatura.toLowerCase(),
+            ...this.getScheduleCalendarEvents(schedule), // start, end, sala
+            color: "purple",
+            class: `text-capitalize`,
+          };
+        });
+    },
+
+    getScheduleCalendarEvents(schedule) {
+      // The early exit will never be executed because we are filtering
+      if (schedule.horario === "0:00:00 - 0:00:00") return null;
+
+      const today = new Date(); // used to calculate current week's monday
+      const monday = new Date(
+        today.getFullYear(),
+        today.getMonth(),
+        today.getDate() - today.getDay() + 1
+      );
+
+      const dayToNumber = {
+        Lunes: 1,
+        Martes: 2,
+        Miercoles: 3,
+        Jueves: 4,
+        Viernes: 5,
+        Sabado: 6,
+      };
+
+      // string without first 3 characters of schedule.horario
+      const [start, end] = schedule.horario.slice(3).split(" - ");
+      return {
+        sala: schedule.sala,
+        start: new Date(
+          monday.getFullYear(),
+          monday.getMonth(),
+          monday.getDate() + dayToNumber[schedule.dia] - 1, // Monday + day of schedule
+          start.split(":")[0],
+          start.split(":")[1]
+        ),
+        end: new Date(
+          monday.getFullYear(),
+          monday.getMonth(),
+          monday.getDate() + dayToNumber[schedule.dia] - 1, // Monday + day of schedule
+          end.split(":")[0],
+          end.split(":")[1]
+        ),
+      };
+    },
   },
 
   async created() {
@@ -65,6 +153,9 @@ export default {
     );
   },
 
+  mounted() {
+    console.log(this.calendarEvents);
+  },
   async beforeUnmount() {
     await this.$store.dispatch("calendars/setSectionsSidebar", false);
   },
