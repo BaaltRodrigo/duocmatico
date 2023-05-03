d8bc3fb19ca2c41929f860deb3ace31bfbd556de
BaaltRodrigo <r.antonio.regueiro@gmail.com>
2023-05-02 23:14:26 -0400

    feat(sections): :sparkles: Add more information to section card
    Also use the section card to display on-click calendar events

--- src/components/sections/DmSectionCard.vue
+++ src/components/sections/DmSectionCard.vue
@@ -1,7 +1,10 @@
 <template>
-  <v-card variant="outlined" class="rounded-xl">
+  <v-card variant="outlined" class="rounded-xl bg-white pa-2">
     <v-list-item class="mt-2">
       <v-list-item-title>{{ section.seccion }}</v-list-item-title>
+      <v-list-item-subtitle class="text-capitalize">
+        {{ section.asignatura.toLowerCase() }}
+      </v-list-item-subtitle>
       <template #append>
         <v-btn
           variant="outlined"
@@ -13,25 +16,103 @@
         </v-btn>
       </template>
     </v-list-item>
-    <v-card-text>
-      <v-table density="compact">
-        <thead>
-          <tr>
-            <th>Dia y Hora</th>
-            <th>Sala</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="(schedule, index) in validSchedules" :key="index">
-            <td>
-              {{ schedule.horario }}
-            </td>
-            <td>{{ schedule.sala }}</td>
-          </tr>
-        </tbody>
-      </v-table>
-      <v-btn variant="tonal" block class="rounded-pill">Ver mas</v-btn>
-    </v-card-text>
+    <v-container fluid>
+      <v-row>
+        <v-col cols="12" :md="isInSelection ? 12 : 6">
+          <v-card variant="outlined" class="rounded-xl">
+            <v-table density="compact">
+              <thead>
+                <tr>
+                  <th>Dia y Hora</th>
+                  <th>Sala</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="(schedule, index) in validSchedules" :key="index">
+                  <td>
+                    {{ schedule.horario }}
+                  </td>
+                  <td>{{ schedule.sala }}</td>
+                </tr>
+              </tbody>
+            </v-table>
+          </v-card>
+        </v-col>
+        <v-expand-transition>
+          <v-col
+            v-show="showMoreInformation"
+            cols="12"
+            :md="isInSelection ? 12 : 6"
+          >
+            <v-card variant="outlined" class="rounded-xl">
+              <v-table density="compact">
+                <!-- make headers on the left side and information on rhe right side -->
+                <tbody>
+                  <tr>
+                    <th>Docente</th>
+                    <td>{{ section.docente ?? "Sin Docente" }}</td>
+                  </tr>
+                  <tr>
+                    <th>Modalidad</th>
+                    <td>
+                      <v-chip
+                        label
+                        variant="outlined"
+                        size="small"
+                        class="text-capitalize"
+                      >
+                        {{ section.tipoPlanEstudios.toLowerCase() }}
+                      </v-chip>
+                    </td>
+                  </tr>
+                  <tr>
+                    <th>Jornada</th>
+                    <td>
+                      <v-chip
+                        label
+                        variant="outlined"
+                        size="small"
+                        class="text-capitalize"
+                      >
+                        {{ section.jornada.toLowerCase() }}
+                      </v-chip>
+                    </td>
+                  </tr>
+                  <tr>
+                    <th>Semestre</th>
+                    <td>{{ section.nivel ?? "Optativo" }}</td>
+                  </tr>
+                  <tr>
+                    <th>Plan de estudios</th>
+                    <td>
+                      <v-chip
+                        v-for="(plan, index) in section.planes"
+                        :key="`${section.seccion}-plan-${index}`"
+                        label
+                        variant="outlined"
+                        size="small"
+                        class="mr-1"
+                      >
+                        {{ plan }}
+                      </v-chip>
+                    </td>
+                  </tr>
+                </tbody>
+              </v-table>
+            </v-card>
+          </v-col>
+        </v-expand-transition>
+      </v-row>
+      <v-btn
+        v-if="!hideAddButton"
+        variant="tonal"
+        class="mt-4 rounded-pill text-capitalize"
+        block
+        @click="showMore = !showMore"
+      >
+        {{ showMoreInformation ? "Ocultar" : "Ver Mas" }}
+      </v-btn>
+    </v-container>
   </v-card>
 </template>
 
@@ -45,6 +126,16 @@ export default {
     section: {
       type: Object,
       default: () => ({}),
+    },
+
+    isInSelection: {
+      type: Boolean,
+      default: false,
+    },
+
+    hideAddButton: {
+      type: Boolean,
+      default: false,
     },
   },
 
@@ -61,7 +152,15 @@ export default {
       const { sections } = this.selectedCalendar;
       return sections.some((s) => s.seccion === section.seccion);
     },
+
+    showMoreInformation() {
+      return this.showMore || !this.isInSelection;
+    },
   },
+
+  data: () => ({
+    showMore: false,
+  }),
 
   methods: {
     ...mapActions("calendars", ["addSection", "removeSection"]),
