14f80f4f4765725fedf3b4ca374a80e69731069f
BaaltRodrigo <r.antonio.regueiro@gmail.com>
2023-05-07 00:10:10 -0400

    Add a winking face to auth alert. Just for fun

--- src/components/auth/WorkInProgressAlert.vue
+++ src/components/auth/WorkInProgressAlert.vue
@@ -6,6 +6,6 @@
     class="text-justify rounded-xl"
   >
     Por el momento solo disponemos de autenticacion con Google, pero
-    proximamente podras registrarte con tu correo y contraseña, entre otros.
+    proximamente podras registrarte con tu correo y contraseña, entre otros. 😉
   </v-alert>
 </template>
