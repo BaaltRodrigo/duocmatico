cc312192275578ef4869e52dd2e5c01aa9197c3f
Rodrigo Pizarro <r.antonio.regueiro@gmail.com>
2023-10-02 12:52:53 -0300

    feat: add 404 error page (#19)
    * Default and not found image
    
    * Components to show 404 view
    
    * Path matching to 404

---
+++ src/components/LeftRightGrid.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-row :justify="justify" align="center">
+    <v-col cols="12" md="6">
+      <slot>
+        <!-- Default slot can be changed in need to render another title type, like h4 -->
+        <h3 class="text-h3">{{ title }}</h3>
+        <br />
+        <p class="text-h5 font-weight-light">{{ description }}</p>
+      </slot>
+    </v-col>
+    <v-col cols="12" md="4" :order="reversed ? 'first' : 'last'">
+      <slot name="media">
+        <!-- The slot can be changed in need to render another media type, like video -->
+        <v-img :src="image" aspect-ratio="16/9" max-height="250"></v-img>
+      </slot>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import defaultImage from "../assets/images/undraw_default.svg";
+
+export default {
+  name: "LeftRightGrid",
+
+  props: {
+    title: {
+      type: String,
+      default: "Title",
+    },
+    description: {
+      type: String,
+      default: "Description",
+    },
+    image: {
+      type: String,
+      default: defaultImage,
+    },
+    reversed: {
+      type: Boolean,
+      default: false,
+    },
+    justify: {
+      type: String,
+      default: "space-between",
+    },
+  },
+};
+</script>
---
+++ src/views/errors/NotFound.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-container class="fill-height">
+    <!-- Double container to get the rounded edges separated from the top and bottom -->
+    <v-container class="fill-height bg-grey-lighten-2 rounded-xl">
+      <left-right-grid justify="space-around" :image="not_found_image" reversed>
+        <v-card
+          variant="outlined"
+          class="rounded-lg pa-4 bg-white"
+          title="No encontramos lo que estas buscando"
+          text="Quizas alguna de estas opciones pueda ayudarte"
+        >
+          <v-card-actions>
+            <v-btn
+              class="text-none"
+              variant="elevated"
+              color="green-accent-1"
+              @click="$router.push({ name: 'calendars.index' })"
+            >
+              Ir a mis calendarios
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </left-right-grid>
+    </v-container>
+  </v-container>
+</template>
+
+<script>
+import undraw_void from "../../assets/images/undraw_void.svg";
+import LeftRightGrid from "../../components/LeftRightGrid.vue";
+
+export default {
+  name: "NotFoundView",
+
+  components: {
+    LeftRightGrid,
+  },
+
+  data: () => ({
+    not_found_image: undraw_void,
+  }),
+};
+</script>
