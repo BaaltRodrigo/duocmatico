2ac3e4a93f946c8a6aed3f3a012050efc01732ce
joakenseg <81238879+joakenseg@users.noreply.github.com>
2023-10-03 13:14:53 -0300

    component to delete calendar

--- src/components/calendar/DmDeleteCalendar.vue
+++ src/components/calendar/DmDeleteCalendar.vue
@@ -1,49 +1,37 @@
 <template>
-  <v-card 
-    class="rounded-xl"
-    variant="outlined"
-    style="background-color: #FFF7EA"
-  >
+  <v-card class="rounded-xl" variant="outlined" style="background-color: #FFF7EA">
     <v-card-text class="text-subtitle-1">
       <v-alert class="rounded-lg" type="warning" variant="tonal" icon="mdi-alert-circle">
         Estas seguro que deseas eliminar este item?
       </v-alert>
       <div class="d-flex justify-space-between mt-4">
-        <v-btn
-          :disabled="false"
-          block
-          color="#FF9C72"
-          variant="flat"
-          class="rounded-pill text-white"
-          @click="deleteCalendar"
-        >
+        <v-btn block color="#FF9C72" variant="flat" class="rounded-pill text-white" @click="confirmDelete">
           Si, eliminar este calendario
         </v-btn>
       </div>
     </v-card-text>
   </v-card>
-
 </template>
 
 <script>
-import { mapActions, mapState } from "vuex";
+import { mapActions } from "vuex";
 
 export default {
   name: "DmDeleteCalendar",
 
-  computed: {
-    
+  props: {
+    calendar: Object,
   },
 
   methods: {
-    close() {
-      this.$emit('close')
+    ...mapActions("calendars", ["removeCalendar"]),
+    confirmDelete() {
+      if (this.calendar) {
+        this.removeCalendar(this.calendar).then(() => {
+          this.$emit("deleted");
+        });
+      }
     },
-
-    deleteItem() {
-      this.$emit('close')
-    }
-  }
-
-}
+  },
+};
 </script>
--- src/views/CalendarIndex.vue
+++ src/views/CalendarIndex.vue
@@ -3,13 +3,8 @@
     <h4 class="text-h4 mb-2">Mis calendarios</h4>
     <v-row>
       <v-col cols="12" md="4">
-        <v-card
-          @click="newCalendarForm = true"
-          variant="flat"
-          height="150px"
-          class="rounded-lg"
-          style="border: 4px  lightgray; background-color: #FF9C72"
-        >
+        <v-card @click="newCalendarForm = true" variant="flat" height="150px" class="rounded-lg"
+          style="border: 4px  lightgray; background-color: #FF9C72">
           <v-container class="text-center text-white mt-4">
             <v-icon size="7vh">mdi-plus</v-icon>
             <h3>Nuevo calendario</h3>
@@ -19,32 +14,22 @@
     </v-row>
     <!-- Get calendars from store and create a card for every one -->
     <v-row>
-      <v-col
-        cols="12"
-        md="4"
-        v-for="(calendar, index) in localCalendars"
-        :key="`calendar-${index}`"
-      >
-        <v-card
-          height="150px"
-          class="rounded-xl"
-          style="background-color: #F0BD6A"
-          @click="
-            $router.push({ name: 'calendars.show', params: { id: index } })
-          "
-        >
+      <v-col cols="12" md="4" v-for="(calendar, index) in localCalendars" :key="`calendar-${index}`">
+        <v-card height="150px" class="rounded-xl" style="background-color: #F0BD6A" @click="
+          $router.push({ name: 'calendars.show', params: { id: index } })
+          ">
           <v-card-title class="ml-2">{{ calendar.name }}</v-card-title>
           <v-card-subtitle class="ml-2 my-n2 text-capitalize">
             {{ `${calendar.carrera} - ${calendar.carga}`.toLowerCase() }}
           </v-card-subtitle>
           <v-card-actions class="float-end mt-8">
-            <v-btn icon>
+            <v-btn icon v-on:click.stop.prevent=true>
               <v-icon>mdi-share-variant</v-icon>
             </v-btn>
-            <v-btn icon v-on:click.stop.prevent="deleteCalendar = true">
+            <v-btn icon v-on:click.stop.prevent="calendarToDelete = calendar; deleteCalendar = true">
               <v-icon>mdi-delete</v-icon>
             </v-btn>
-            <v-btn icon color="grey-lighten-5" variant="flat">
+            <v-btn icon color="grey-lighten-5" variant="flat" v-on:click.stop.prevent=true>
               <v-icon>mdi-pencil</v-icon>
             </v-btn>
           </v-card-actions>
@@ -52,27 +37,17 @@
       </v-col>
     </v-row>
   </v-container>
-  <v-dialog
-    v-model="newCalendarForm"
-    max-width="480px"
-    content-class="elevation-0"
-    hide-overlay
-  >
+  <v-dialog v-model="newCalendarForm" max-width="480px" content-class="elevation-0" hide-overlay>
     <dm-calendar-form @created="newCalendarForm = false"></dm-calendar-form>
   </v-dialog>
 
-  <v-dialog
-    v-model="deleteCalendar"
-    max-width="530px"
-    content-class="elevation-0"
-    hide-overlay
-  >
-    <dm-delete-calendar @created="deleteCalendar = false"></dm-delete-calendar>
+  <v-dialog v-model="deleteCalendar" max-width="530px" content-class="elevation-0" hide-overlay>
+    <dm-delete-calendar @deleted="deleteCalendar" :calendar="calendarToDelete"></dm-delete-calendar>
   </v-dialog>
 </template>
 
 <script>
-import { mapState, mapAction } from "vuex";
+import { mapState } from "vuex";
 import DmCalendarForm from "../components/calendar/DmCalendarForm.vue";
 import DmDeleteCalendar from "../components/calendar/DmDeleteCalendar.vue";
 
@@ -88,17 +63,9 @@ export default {
     ...mapState("calendars", ["localCalendars"]),
   },
 
-  methods:{
-    ...mapAction("calendars", ["removeCalendar"]),
-
-    deleteCalendar(){
-      
-    }
-  },
-
   data: () => ({
     newCalendarForm: false,
-    deleteCalendar: false,
+    deleteCalendar: null,
   }),
 };
 </script>
