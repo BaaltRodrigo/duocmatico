c3e2e5c8a39bd3e2d59f2b9eb4666e2f142d1cb0
cristobalwalters12 <cristobalwalters@gmail.com>
2023-10-03 23:30:53 -0300

    Add: store of login , registration and passwordRequest
    asa

--- src/store/auth.js
+++ src/store/auth.js
@@ -7,6 +7,9 @@ import {
   setPersistence,
   signInWithPopup,
   signOut,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  sendPasswordResetEmail,
 } from "firebase/auth";
 
 const state = {
@@ -41,24 +44,25 @@ const actions = {
   },
 
   async updateFirebaseUser({ commit }, user) {
-    // get user from firebase collection with user uuid
     const userRef = doc(db, "users", user.uid);
+
+    // Obtienes userDoc aquí
     const userDoc = await getDoc(userRef);
 
     if (!userDoc.exists()) {
-      // create new user with base roles
       await setDoc(userRef, {
         name: user.displayName,
         email: user.email,
-        photoURL: user?.photoURL,
+        photoURL: user.photoURL,
       });
+      user.photoURL = user.photoURL;
     } else {
-      // update user, but not the roles
       await updateDoc(userRef, {
         name: user.displayName,
         email: user.email,
-        photoURL: user?.photoURL,
+        photoURL: user.photoURL,
       });
+      user.photoURL = user.photoURL;
     }
     commit("setUser", user);
   },
@@ -67,7 +71,7 @@ const actions = {
     try {
       const provider = new GoogleAuthProvider();
       const auth = getAuth();
-      await setPersistence(auth, browserLocalPersistence); // Keep user logged in.
+      await setPersistence(auth, browserLocalPersistence);
       const response = await signInWithPopup(auth, provider);
       console.log(response.user);
       commit("setUser", response.user);
@@ -77,11 +81,21 @@ const actions = {
     }
   },
 
-  async login({ commit }, { email, password }) {
-    const { user } = await firebase
-      .auth()
-      .signInWithEmailAndPassword(email, password);
-    commit("setUser", user);
+  async login({ commit, dispatch }, { email, password }) {
+    try {
+      const auth = getAuth();
+      await setPersistence(auth, browserLocalPersistence);
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      console.log(userCredential.user);
+      commit("setUser", userCredential.user);
+      dispatch("updateFirebaseUser", userCredential.user);
+    } catch (error) {
+      console.error(error);
+    }
   },
 
   async logout({ commit }) {
@@ -89,12 +103,31 @@ const actions = {
     await signOut(auth);
     commit("setUser", null);
   },
-
-  async register({ commit }, { email, password }) {
-    const { user } = await firebase
-      .auth()
-      .createUserWithEmailAndPassword(email, password);
-    commit("setUser", user);
+  async registration(
+    { commit, dispatch },
+    { email, password, passwordConfirmation }
+  ) {
+    if (password !== passwordConfirmation) {
+      throw new Error("Passwords do not match");
+    }
+    const auth = getAuth();
+    const userCredential = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    commit("setUser", userCredential.user);
+    dispatch("updateFirebaseUser", userCredential.user);
+  },
+  async requestPasswordReset({ commit }, email) {
+    try {
+      const auth = getAuth();
+      const resetResponse = await sendPasswordResetEmail(auth, email);
+      return resetResponse;
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
   },
 };
 
