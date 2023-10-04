cbe425b4f92d1f663b05cbbd8d447995015abbb7
Joaquin Romero <81238879+joakenseg@users.noreply.github.com>
2023-10-04 00:56:04 -0300

    feat: Options to rename and delete calendars (#21)
    * se modificaron las vistas y se elimino un componente que no se utilizaba
    
    * button disabled
    
    * checkpoint
    
    * component to delete calendar
    
    * delete dialog fixed and edit calendar name created
    
    * problems fixed

--- src/components/calendar/DmCalendarForm.vue
+++ src/components/calendar/DmCalendarForm.vue
@@ -1,40 +1,17 @@
 <template>
-  <v-card
-    class="rounded-xl"
-    variant="outlined"
-    style="background-color: #f5f5f5"
-    title="Nuevo Calendario"
-  >
+  <v-card class="rounded-xl" title="Nuevo Calendario">
     <v-card-text>
       <v-form ref="form">
-        <v-text-field
-          v-model="name"
-          filled
-          variant="outlined"
-          label="Nombre del calendario"
-        ></v-text-field>
-        <v-autocomplete
-          v-model="cargaForm"
-          :items="cargasAcademicas"
-          label="Carga academica"
-          variant="outlined"
-        ></v-autocomplete>
-        <v-autocomplete
-          v-model="careerForm"
-          :items="carreras"
-          label="Carrera"
-          variant="outlined"
-        ></v-autocomplete>
+        <v-text-field v-model="name" filled variant="outlined" label="Nombre del calendario"
+          bg-color="white"></v-text-field>
+        <v-autocomplete v-model="cargaForm" :items="cargasAcademicas" label="Carga academica" variant="outlined"
+          bg-color="white"></v-autocomplete>
+        <v-autocomplete v-model="careerForm" :items="carreras" label="Carrera" variant="outlined"
+          bg-color="white"></v-autocomplete>
       </v-form>
       <v-card-actions>
-        <v-btn
-          :disabled="isDisabled"
-          block
-          color="purple"
-          variant="outlined"
-          class="rounded-pill"
-          @click="createCalendar"
-        >
+        <v-btn :disabled="isDisabled" block color="green-accent-1" variant="flat" class="rounded-pill text-white"
+          @click="createCalendar">
           Empieza a armar tu horario!
         </v-btn>
       </v-card-actions>
@@ -77,8 +54,8 @@ export default {
     ...mapActions("calendars", ["addCalendar"]),
 
     async changeCargaAcademica() {
-      this.careerForm = null; // Esto ocurre siempre que cambia la carga academica
-      if (!this.cargaForm) return; // Carga academica es borrada -> no hago nada
+      this.careerForm = null;
+      if (!this.cargaForm) return;
       await this.setCargaAcademica(this.cargaForm);
     },
 
--- src/views/CalendarIndex.vue
+++ src/views/CalendarIndex.vue
@@ -3,71 +3,89 @@
     <h4 class="text-h4 mb-2">Mis calendarios</h4>
     <v-row>
       <v-col cols="12" md="4">
-        <v-card
-          @click="newCalendarForm = true"
-          variant="flat"
-          height="16vh"
-          class="rounded-lg"
-          style="border: 4px dashed lightgray; background-color: transparent"
-        >
-          <v-container class="text-center">
+        <v-card @click="newCalendarForm = true" variant="flat" height="150px" class="rounded-xl"
+          style="border: 4px dashed lightgray; background-color: transparent">
+          <v-container class="text-center mt-4">
             <v-icon size="7vh">mdi-plus</v-icon>
             <h3>Nuevo calendario</h3>
           </v-container>
         </v-card>
       </v-col>
     </v-row>
-    <!-- Get calendars from store and create a card for every one -->
     <v-row>
-      <v-col
-        cols="12"
-        md="4"
-        v-for="(calendar, index) in localCalendars"
-        :key="`calendar-${index}`"
-      >
-        <v-card
-          height="16vh"
-          variant="outlined"
-          class="rounded-xl elevation-4"
-          @click="
-            $router.push({ name: 'calendars.show', params: { id: index } })
-          "
-        >
+      <v-col cols="12" md="4" v-for="(calendar, index) in localCalendars" :key="`calendar-${index}`">
+        <v-card height="150px" class="rounded-xl elevation-4" variant="outlined"
+          @click="$router.push({ name: 'calendars.show', params: { id: index } })">
           <v-card-title class="ml-2">{{ calendar.name }}</v-card-title>
           <v-card-subtitle class="ml-2 my-n2 text-capitalize">
             {{ `${calendar.carrera} - ${calendar.carga}`.toLowerCase() }}
           </v-card-subtitle>
+          <v-card-actions class="float-end mt-8">
+            <v-btn icon @click.stop.prevent=true>
+              <v-icon>mdi-share-variant</v-icon>
+            </v-btn>
+            <v-btn icon @click.stop.prevent="calendarToDelete = calendar; deleteCalendar = true">
+              <v-icon>mdi-delete</v-icon>
+            </v-btn>
+            <v-btn icon color="grey-lighten-5" variant="flat" @click.stop.prevent="openEditCalendarNameCard(calendar)">
+              <v-icon>mdi-pencil</v-icon>
+            </v-btn>
+          </v-card-actions>
         </v-card>
       </v-col>
     </v-row>
   </v-container>
-  <v-dialog
-    v-model="newCalendarForm"
-    max-width="480px"
-    content-class="elevation-0"
-    hide-overlay
-  >
+  <v-dialog v-model="newCalendarForm" max-width="480px" content-class="elevation-0" hide-overlay>
     <dm-calendar-form @created="newCalendarForm = false"></dm-calendar-form>
+  </v-dialog>
+
+  <v-dialog v-model="deleteCalendar" max-width="530px" content-class="elevation-0" hide-overlay>
+    <dm-delete-calendar @deleted-successfully="deleteCalendar = false" :calendar="calendarToDelete"></dm-delete-calendar>
+  </v-dialog>
+
+  <v-dialog v-model="editCalendarName" max-width="480px" content-class="elevation-0" hide-overlay>
+    <DmEditCalendarName :calendar="calendarEditName" @updated="nameUpdated" />
   </v-dialog>
 </template>
 
 <script>
 import { mapState } from "vuex";
 import DmCalendarForm from "../components/calendar/DmCalendarForm.vue";
+import DmDeleteCalendar from "../components/calendar/DmDeleteCalendar.vue";
+import DmEditCalendarName from "../components/calendar/DmEditCalendarName.vue";
 
 export default {
   name: "CalendarIndexView",
 
   components: {
     DmCalendarForm,
-  },
-
-  computed: {
-    ...mapState("calendars", ["localCalendars"]),
+    DmDeleteCalendar,
+    DmEditCalendarName,
   },
 
   data: () => ({
     newCalendarForm: false,
+    deleteCalendar: false,
+    editCalendarName: false,
+    calendarToEditName: null,
   }),
+
+  computed: {
+    ...mapState("calendars", ["localCalendars"]),
+    calendarEditName() {
+      return this.localCalendars.find((c) => c === this.calendarToEditName);
+    },
+  },
+
+  methods: {
+    openEditCalendarNameCard(calendar) {
+      this.calendarToEditName = calendar;
+      this.editCalendarName = true;
+    },
+    nameUpdated() {
+      this.editCalendarName = false;
+    },
+  },
 };
 </script>
+
