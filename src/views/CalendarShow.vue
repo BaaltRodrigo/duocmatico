6ce1b48deeb5412569710007ac7f7556833ee48c
Rodrigo Pizarro <r.antonio.regueiro@gmail.com>
2023-10-13 00:03:08 -0300

    vue-cal events now renders section resources

--- src/views/CalendarShow.vue
+++ src/views/CalendarShow.vue
@@ -2,7 +2,11 @@
   <v-container v-if="calendar">
     <h4 class="text-h4 mb-2">{{ calendar.name }}</h4>
 
-    <v-card class="rounded-xl elevation-0" variant="outlined" height="70vh">
+    <v-card
+      class="my-4 rounded-xl elevation-0"
+      variant="outlined"
+      height="70vh"
+    >
       <vue-cal
         hide-view-selector
         hide-title-bar
@@ -13,6 +17,7 @@
         locale="es"
         :events="calendarEvents"
       >
+        <!-- This slot is how every calendar event should render -->
         <template v-slot:event="{ event }">
           <v-card
             class="rounded-lg"
@@ -24,21 +29,19 @@
             <v-card-title class="text-capitalize text-body-2">
               {{ event.title }}
             </v-card-title>
-            <v-card-text>
+            <v-card-text class="text-body-2">
               {{ event.start.toTimeString().slice(0, 5) }} -
               {{ event.end.toTimeString().slice(0, 5) }}
               <br />
-              Sala: {{ event.sala }}
+              Sala: {{ event.classroom.toUpperCase() }}
             </v-card-text>
           </v-card>
         </template>
       </vue-cal>
     </v-card>
-    <br />
     <v-btn
-      variant="outlined"
-      class="rounded-pill text-capitalize"
-      color="primary"
+      class="rounded-xl text-none"
+      color="green-accent-1"
       @click="handleGetSections()"
     >
       Agregar secciones
@@ -109,27 +112,27 @@ export default {
     },
 
     getSectionEvents(section) {
-      const { horarios } = section;
-      return horarios
-        .filter((h) => h.horario != "0:00:00 - 0:00:00") // Deleting non valid schedules
+      const { schedules } = section;
+      return schedules
+        .filter((schedule) => schedule.times != "0:00:00 - 0:00:00") // Deleting non valid schedules
         .map((schedule) => {
           // const { start, end } = this.getScheduleCalendarEvents(schedule);
           // For some reason, there is no need to subtract the timezone offset
           // start.setHours(start.getHours() - start.getTimezoneOffset() / 60);
           // end.setHours(end.getHours() - end.getTimezoneOffset() / 60);
           return {
-            title: section.asignatura.toLowerCase(),
+            title: section.subject.name.toLowerCase(),
             ...this.getScheduleCalendarEvents(schedule), // start, end, sala
             color: "purple",
             class: `text-capitalize`,
-            sectionId: section.seccion,
+            sectionId: section.code,
           };
         });
     },
 
     getScheduleCalendarEvents(schedule) {
       // The early exit will never be executed because we are filtering
-      if (schedule.horario === "0:00:00 - 0:00:00") return null;
+      if (schedule.times === "0:00:00 - 0:00:00") return null;
 
       const today = new Date(); // used to calculate current week's monday
       const monday = new Date(
@@ -139,29 +142,29 @@ export default {
       );
 
       const dayToNumber = {
-        Lunes: 1,
-        Martes: 2,
-        Miercoles: 3,
-        Jueves: 4,
-        Viernes: 5,
-        Sabado: 6,
+        lunes: 1,
+        martes: 2,
+        miercoles: 3,
+        jueves: 4,
+        viernes: 5,
+        sabado: 6,
       };
 
       // string without first 3 characters of schedule.horario
-      const [start, end] = schedule.horario.slice(3).split(" - ");
+      const [start, end] = schedule.times.slice(3).split(" - ");
       return {
-        sala: schedule.sala,
+        classroom: schedule.classroom,
         start: new Date(
           monday.getFullYear(),
           monday.getMonth(),
-          monday.getDate() + dayToNumber[schedule.dia] - 1, // Monday + day of schedule
+          monday.getDate() + dayToNumber[schedule.day] - 1, // Monday + day of schedule
           start.split(":")[0],
           start.split(":")[1]
         ),
         end: new Date(
           monday.getFullYear(),
           monday.getMonth(),
-          monday.getDate() + dayToNumber[schedule.dia] - 1, // Monday + day of schedule
+          monday.getDate() + dayToNumber[schedule.day] - 1, // Monday + day of schedule
           end.split(":")[0],
           end.split(":")[1]
         ),
