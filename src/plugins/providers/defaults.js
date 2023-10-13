50b856f96d9222bccf28e9faa06e61ba12c07aec
Rodrigo Pizarro <r.antonio.regueiro@gmail.com>
2023-10-13 00:46:41 -0300

    Base default style provider

---
+++ src/plugins/providers/defaults.js
@@ -0,0 +1,11 @@
+const base = {
+  VBtn: {
+    class: "text-none",
+  },
+
+  VCard: {
+    variant: "outlined",
+  },
+};
+
+export { base };
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -7,8 +7,14 @@ import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
 
+// Providers
+import { base } from "./providers/defaults";
+
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
   components,
   directives,
+  defaults: {
+    ...base,
+  },
 });
