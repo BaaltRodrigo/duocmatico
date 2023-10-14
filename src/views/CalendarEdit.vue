eb9c558afc27fe65391840164f32fdff2d8dae09
joakenseg <81238879+joakenseg@users.noreply.github.com>
2023-10-14 01:11:37 -0300

    vista para editar el calendario

---
+++ src/views/CalendarEdit.vue
@@ -0,0 +1,184 @@
+<template>
+  <v-container v-if="calendar">
+    <DMEditSidebar :calendar="calendar" />
+    <h4 class="text-h4 mb-2">{{ calendar.name }}</h4>
+    <v-card class="rounded-xl elevation-0" variant="outlined" height="70vh">
+      <vue-cal hide-view-selector hide-title-bar :disable-views="['years', 'year', 'month', 'day']" :hide-weekdays="[7]"
+        :time-from="8 * 60" :time-step="30" locale="es" :events="calendarEvents">
+        <template v-slot:event="{ event }">
+          <v-card class="rounded-lg" variant="tonal" height="100%" :color="event.color"
+            @click="openSectionInformation(event.sectionId)">
+            <v-card-title class="text-capitalize text-body-2">
+              {{ event.title }}
+            </v-card-title>
+            <v-card-text>
+              {{ event.start.toTimeString().slice(0, 5) }} -
+              {{ event.end.toTimeString().slice(0, 5) }}
+              <br />
+              Sala: {{ event.sala }}
+            </v-card-text>
+          </v-card>
+        </template>
+      </vue-cal>
+    </v-card>
+    <br />
+
+    <v-btn variant="outlined" class="rounded-pill text-capitalize" color="primary" @click=$router.go(-1)>
+      Finalizar
+    </v-btn>
+  </v-container>
+</template>
+  
+<script>
+import { mapActions, mapState } from "vuex";
+import { useDisplay } from "vuetify";
+import VueCal from "vue-cal";
+import "vue-cal/dist/vuecal.css";
+import DmSectionCard from "../components/sections/DmSectionCard.vue";
+import DmEditCalendarName from "../components/calendar/DmEditCalendarName.vue";
+import DMEditSidebar from "../components/calendar/DMEditSidebar.vue"
+
+export default {
+  name: "CalendarShow",
+
+  components: {
+    VueCal,
+    DmSectionCard,
+    DmEditCalendarName,
+    DMEditSidebar
+  },
+
+  computed: {
+    ...mapState("calendars", ["calendar"]),
+    ...mapState("academicCharges", ["sections"]),
+
+    calendarEvents() {
+      return this.getCalendarEvents();
+    },
+
+    isMobile() {
+      const { mobile } = useDisplay();
+      return mobile.value;
+    },
+  },
+
+  data: () => ({
+    loaded: false,
+    sectionInformation: false,
+    section: null,
+  }),
+
+  methods: {
+    ...mapActions("academicCharges", ["getSections"]),
+
+    // An example of how to get sections
+    handleGetSections() {
+      this.$store.dispatch("academicCharges/getSections", {
+        academicChargeId: this.calendar.academic_charge.id,
+        calendarableType: this.calendar.calendarable_type,
+        calendarableId: this.calendar.calendarable.id,
+      });
+
+    },
+
+    openSectionInformation(sectionId) {
+      this.section = this.secciones.find(
+        (section) => section.seccion === sectionId
+      );
+      this.$nextTick(() => (this.sectionInformation = true));
+    },
+
+    getCalendarEvents() {
+      const { sections } = this.calendar;
+      return sections.map((section) => this.getSectionEvents(section)).flat();
+    },
+
+    getSectionEvents(section) {
+      const { horarios } = section;
+      return horarios
+        .filter((h) => h.horario != "0:00:00 - 0:00:00") // Deleting non valid schedules
+        .map((schedule) => {
+          // const { start, end } = this.getScheduleCalendarEvents(schedule);
+          // For some reason, there is no need to subtract the timezone offset
+          // start.setHours(start.getHours() - start.getTimezoneOffset() / 60);
+          // end.setHours(end.getHours() - end.getTimezoneOffset() / 60);
+          return {
+            title: section.asignatura.toLowerCase(),
+            ...this.getScheduleCalendarEvents(schedule), // start, end, sala
+            color: "purple",
+            class: `text-capitalize`,
+            sectionId: section.seccion,
+          };
+        });
+    },
+
+    getScheduleCalendarEvents(schedule) {
+      // The early exit will never be executed because we are filtering
+      if (schedule.horario === "0:00:00 - 0:00:00") return null;
+
+      const today = new Date(); // used to calculate current week's monday
+      const monday = new Date(
+        today.getFullYear(),
+        today.getMonth(),
+        today.getDate() - today.getDay() + 1
+      );
+
+      const dayToNumber = {
+        Lunes: 1,
+        Martes: 2,
+        Miercoles: 3,
+        Jueves: 4,
+        Viernes: 5,
+        Sabado: 6,
+      };
+
+      // string without first 3 characters of schedule.horario
+      const [start, end] = schedule.horario.slice(3).split(" - ");
+      return {
+        sala: schedule.sala,
+        start: new Date(
+          monday.getFullYear(),
+          monday.getMonth(),
+          monday.getDate() + dayToNumber[schedule.dia] - 1, // Monday + day of schedule
+          start.split(":")[0],
+          start.split(":")[1]
+        ),
+        end: new Date(
+          monday.getFullYear(),
+          monday.getMonth(),
+          monday.getDate() + dayToNumber[schedule.dia] - 1, // Monday + day of schedule
+          end.split(":")[0],
+          end.split(":")[1]
+        ),
+      };
+    },
+  },
+
+  /**
+   * Here we check everything we need to show the calendar
+   * If something is missing, we get it from the API
+   * If something is missing on the API, we show an error
+   */
+  async created() {
+    const { uuid } = this.$route.params;
+    // Calendar is inside local calendars, use that one
+    await this.$store.dispatch("calendars/getLocalCalendarByUuid", uuid);
+    if (this.calendar) return;
+
+    // Calendar is inside API calendars, use that one
+    await this.$store.dispatch("calendars/getApiCalendarByUuid", uuid);
+    if (this.calendar) return;
+
+    // Calendar is not in local or API calendars, show error
+    this.$store.commit("calendars/setCalendar", null);
+    this.$store.commit("set404", true);
+  },
+};
+</script>
+  
+  <!-- Similar border radius to rounded-lg -->
+<style>
+.vuecal__event {
+  border-radius: 0.5rem;
+}
+</style>
