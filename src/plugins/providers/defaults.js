e4235929fac1e8cc27808effe841dd2401aa066f
Rodrigo Pizarro <r.antonio.regueiro@gmail.com>
2023-10-14 14:52:10 -0300

    Header button defaults

--- src/plugins/providers/defaults.js
+++ src/plugins/providers/defaults.js
@@ -1,21 +1,34 @@
 const base = {
+  VAppBar: {
+    color: "transparent",
+    flat: true,
+
+    VBtn: {
+      class: "text-none elevation-0",
+    },
+  },
+
   VAutocomplete: {
     variant: "outlined",
+    density: "comfortable",
   },
 
   VDialog: {
     width: 500,
     contentClass: "elevation-0",
     hideOverlay: true,
+    VCard: {
+      class: "bg-white rounded-xl elevation-0",
+    },
   },
 
   VBtn: {
-    class: "text-none",
+    class: "text-none bg-primary rounded-lg",
   },
 
   VCard: {
     variant: "outlined",
-    class: "bg-white rounded-lg elevation-2",
+    class: "bg-white rounded-lg elevation-4",
   },
 
   VMenu: {
@@ -26,6 +39,7 @@ const base = {
 
   VTextField: {
     variant: "outlined",
+    density: "comfortable",
   },
 };
 
