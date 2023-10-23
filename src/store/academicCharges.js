5edaef5c96726a17c0fa35a46edaa1fba53cef6d
Rodrigo Pizarro <r.antonio.regueiro@gmail.com>
2023-10-23 02:07:50 -0300

    Store can update and upload file

--- src/store/academicCharges.js
+++ src/store/academicCharges.js
@@ -11,6 +11,17 @@ const mutations = {
     state.academicCharges = charges;
   },
 
+  addCharge(state, charge) {
+    state.academicCharges.push(charge);
+  },
+
+  updateCharge(state, charge) {
+    const index = state.academicCharges.findIndex(
+      (item) => item.id === charge.id
+    );
+    state.academicCharges.splice(index, 1, charge);
+  },
+
   setCharge(state, charge) {
     state.academicCharge = charge;
   },
@@ -21,8 +32,12 @@ const mutations = {
 };
 
 const actions = {
-  async getAcademicCharges({ rootState, commit }) {
-    const response = await axios.get(`${rootState.apiUrl}/academic-charges`);
+  async getAcademicCharges({ rootState, commit }, payload = {}) {
+    const response = await axios.get(`${rootState.apiUrl}/academic-charges`, {
+      params: {
+        ...payload,
+      },
+    });
 
     commit("setCharges", response.data.charges);
     commit("addLogEvent", `Academic charges loaded from the API`, {
@@ -48,7 +63,56 @@ const actions = {
    * This return may change to a response for a job in the queue
    * @return { Promise } The response from the API
    */
-  async uploadAcademicChargeFile({ rootState, commit }, payload) {},
+  async uploadAcademicChargeFile({ rootState, commit }, payload) {
+    if (!payload) {
+      // Use the error to show a message to the user
+      commit("addLogEvent", `Missing params for uploadAcademicChargeFile`, {
+        root: true,
+      });
+      return;
+    }
+
+    const { token } = rootState.auth;
+    const route = `${rootState.apiUrl}/academic-charges/`;
+
+    // send the payload as a file to the api route using token as a bearer
+    const response = await axios.post(route, payload, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "multipart/form-data",
+      },
+    });
+
+    commit("addLogEvent", `Academic charge file uploaded to the API`, {
+      root: true,
+    });
+    commit("addCharge", response.data);
+  },
+
+  async updateAcademicCharge({ rootState, commit }, payload) {
+    if (!payload.id) {
+      // Use the error to show a message to the user
+      commit("addLogEvent", `Missing params for updateAcademicCharge`, {
+        root: true,
+      });
+      return;
+    }
+
+    const { token } = rootState.auth;
+    const route = `${rootState.apiUrl}/academic-charges/${payload.id}`;
+
+    const response = await axios.patch(route, payload, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    commit("addLogEvent", `Academic charge updated to the API`, {
+      root: true,
+    });
+
+    commit("setCharge", response.data);
+  },
 
   /**
    * This action is used when we want to add sections to a calendar.
