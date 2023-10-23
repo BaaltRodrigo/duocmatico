3e5bd32066a398b0b9aad98802f2fc6584f7a233
Rodrigo Pizarro <r.antonio.regueiro@gmail.com>
2023-10-23 02:08:42 -0300

    Index show if a charge can be seen by students and update it

--- src/views/files/FilesIndex.vue
+++ src/views/files/FilesIndex.vue
@@ -1,6 +1,6 @@
 <template>
   <v-container>
-    <v-card>
+    <v-card :loading="loading">
       <v-toolbar color="transparent">
         <v-toolbar-title>Cargas academicas</v-toolbar-title>
         <v-spacer></v-spacer>
@@ -14,14 +14,20 @@
           <tr>
             <th class="text-left">Sede</th>
             <th class="text-left">Temporada academica</th>
-            <th class="text-left">Visible</th>
+            <th class="text-left">Disponible para los alumnos</th>
           </tr>
         </thead>
         <tbody>
           <tr v-for="charge in academicCharges" :key="charge.id">
             <td class="text-capitalize">{{ charge.name.toLowerCase() }}</td>
             <td>{{ charge.season }}</td>
-            <td>{{ charge["is_hidden"] }}</td>
+            <td>
+              <v-checkbox
+                :model-value="!charge['is_hidden']"
+                color="info"
+                @click="handleUpdate(charge)"
+              ></v-checkbox>
+            </td>
           </tr>
         </tbody>
       </v-table>
@@ -44,7 +50,7 @@
 </template>
 
 <script>
-import { mapState } from "vuex";
+import { mapActions, mapState } from "vuex";
 import AcademicChargeForm from "../../components/charge/AcademicChargeForm.vue";
 
 export default {
@@ -56,14 +62,33 @@ export default {
 
   data: () => ({
     showForm: false,
+    loading: false,
   }),
 
   computed: {
     ...mapState("academicCharges", ["academicCharges"]),
   },
 
+  methods: {
+    ...mapActions("academicCharges", ["updateAcademicCharge"]),
+
+    async handleUpdate(charge) {
+      this.loading = true;
+
+      await this.updateAcademicCharge({
+        ...charge,
+        is_hidden: !charge.is_hidden,
+      });
+
+      this.loading = false;
+    },
+  },
+
   mounted() {
-    this.$store.dispatch("academicCharges/getAcademicCharges");
+    this.$store.dispatch("academicCharges/getAcademicCharges", {
+      showHidden: true,
+    });
+    console.table(this.academicCharges);
   },
 };
 </script>
