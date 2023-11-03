83fdf45d4ce56b6efb76e11ef38ca4b14fc10636
Joaquin Romero <81238879+joakenseg@users.noreply.github.com>
2023-11-03 01:15:01 -0300

    feat: option to filter sections when editing a calendar (#45)
    * se modificaron las vistas y se elimino un componente que no se utilizaba
    
    * button disabled
    
    * checkpoint
    
    * component to delete calendar
    
    * delete dialog fixed and edit calendar name created
    
    * problems fixed
    
    * unused files were deleted
    
    * se añadio la ruta de la vista para editar el calendario
    
    * vista para editar el calendario
    
    * boton agregar secciones redirige hacia vista de calendarEdit
    
    * vista y sidebar de calendarEdit
    
    * se reemplazo este componente
    
    * sidebar para agregar asignaturas
    
    * fixed
    
    * buttons fixed and delete logs
    
    * filters created
    
    * fixed

--- src/components/sections/DmEditSidebar.vue
+++ src/components/sections/DmEditSidebar.vue
@@ -1,8 +1,25 @@
 <template>
   <v-navigation-drawer class="px-2" floating :width="450" color="transparent">
     <v-card class="elevation-0">
+      <v-container>
+        <v-text-field hide-details v-model="search" prepend-inner-icon="mdi-magnify" clearable label="Asignatura"
+          type="text" variant="outlined" :append-inner-icon="'mdi-cog-outline'"
+          @click:appendInner="showFilter = !showFilter" @click:clear="clearFilters">
+        </v-text-field>
+      </v-container>
+      <v-expand-transition v-show="showFilter">
+        <v-card-text class="text-subtitle-1">
+          <v-autocomplete v-model="selectedLevel" chips closable-chips multiple clearable :items="availableLevels"
+            label="Seleccione el semestre"></v-autocomplete>
+          <v-radio-group inline v-model="selectedShift">
+            <v-radio label="Diurno" value="diurno"></v-radio>
+            <v-radio class="mx-auto" label="Vespertino" value="vespertino"></v-radio>
+          </v-radio-group>
+          <v-btn @click="clearFilters">Limpiar Filtros</v-btn>
+        </v-card-text>
+      </v-expand-transition>
       <v-expansion-panels v-if="groupedSections" variant="accordion">
-        <v-expansion-panel v-for="(group, subjectId) in groupedSections" :key="subjectId">
+        <v-expansion-panel v-for="(group, subjectId) in filteredSubjects" :key="subjectId">
           <v-expansion-panel-title expand-icon="mdi-menu-down" class="text-body-1">
             {{ formatGroupName(group.subject.name) }}
           </v-expansion-panel-title>
@@ -22,7 +39,11 @@ import DmSectionCard from './DmSectionCard.vue';
 
 export default {
   data: () => ({
+    search: "",
     show: false,
+    showFilter: false,
+    selectedLevel: [],
+    selectedShift: ''
   }),
 
   components: {
@@ -31,18 +52,43 @@ export default {
 
   computed: {
     ...mapState('academicCharges', ['sections']),
+
+    availableLevels() {
+      const levels = this.sections.map((s) => s.subject.level)
+
+      const uniqueLevels = [...new Set(levels)]
+
+      return uniqueLevels.sort().map((i) => ({ value: i, title: i == 0 ? 'Optativo' : i }));
+    },
+
     groupedSections() {
       return this.sections.reduce((acc, section) => {
         const subjectId = section.subject.id;
-        if (!acc[subjectId]) {
-          acc[subjectId] = {
-            subject: section.subject,
-            sections: [],
-          };
+        if (!this.selectedShift || this.selectedShift === section.shift) {
+          if (!acc[subjectId]) {
+            acc[subjectId] = {
+              subject: section.subject,
+              sections: [],
+            };
+          }
+          acc[subjectId].sections.push(section);
         }
-        acc[subjectId].sections.push(section);
         return acc;
       }, {});
+    },
+
+    filteredSubjects() {
+      return Object.values(this.groupedSections).filter(group => {
+        const searchTerm = this.search.toLowerCase().replace(/[-\s]/g, '');
+        const subjectName = group.subject.name.toLowerCase().replace(/[-\s]/g, '');
+        const isMatch = subjectName.includes(searchTerm);
+
+        const hasLevelFilter = this.selectedLevel.length === 0 || this.selectedLevel.includes(group.subject.level);
+
+        const hasShiftFilter = !this.selectedShift || group.sections.some(section => this.selectedShift === section.shift);
+
+        return isMatch && hasLevelFilter && hasShiftFilter;
+      });
     },
   },
 
@@ -62,9 +108,15 @@ export default {
       return sentences.join(" ");
     },
 
+    clearFilters() {
+      this.search = '';
+      this.selectedLevel = [];
+      this.selectedShift = null;
+    },
+
   },
-  created() {
-    this.getSections({
+  async created() {
+    await this.getSections({
       academicChargeId: this.calendar.academic_charge.id,
       calendarableId: this.calendar.calendarable.id,
       calendarableType: this.calendar.calendarable_type.toLowerCase(),
