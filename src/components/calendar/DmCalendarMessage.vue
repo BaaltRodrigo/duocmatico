14af44e42eb06d623e26466a4ff74d0b9889bf05
cristobalwalters12 <cristobalwalters@gmail.com>
2023-11-05 16:31:58 -0300

    Update DmCalendarMessage.vue with successful save message

--- src/components/calendar/DmCalendarMessage.vue
+++ src/components/calendar/DmCalendarMessage.vue
@@ -1,9 +1,7 @@
 <template>
   <div class="text-center">
     <v-card>
-      <v-card-text>
-        {{ message }}
-      </v-card-text>
+      <v-card-text> Calendario guardado exitosamente. </v-card-text>
       <v-card-actions>
         <v-btn color="primary" block @click="closeDialog">Aceptar</v-btn>
       </v-card-actions>
@@ -12,25 +10,14 @@
 </template>
 
 <script>
-  export default {
-    computed: {
-      error() {
-        return this.$store.state.calendars.error;
-      },
-      success() {
-        return this.$store.state.calendars.success;
-      },
-      message() {
-        return this.error || this.success;
-      },
+export default {
+  data() {
+    return {};
+  },
+  methods: {
+    closeDialog() {
+      this.$emit("close");
     },
-    data() {
-      return {};
-    },
-    methods: {
-      closeDialog() {
-        this.$emit("close");
-      },
-    },
-  };
+  },
+};
 </script>
