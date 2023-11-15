a1448c7731e963bec364230c91123c7f3021bc55
Rodrigo Pizarro <r.antonio.regueiro@gmail.com>
2023-11-14 23:51:09 -0300

    fix getSections params for new calendar object

--- src/components/sections/DmEditSidebar.vue
+++ src/components/sections/DmEditSidebar.vue
@@ -2,30 +2,60 @@
   <v-navigation-drawer class="px-2" floating :width="450" color="transparent">
     <v-card class="elevation-0">
       <v-container>
-        <v-text-field hide-details v-model="search" prepend-inner-icon="mdi-magnify" clearable label="Asignatura"
-          type="text" variant="outlined" :append-inner-icon="'mdi-cog-outline'"
-          @click:appendInner="showFilter = !showFilter" @click:clear="clearFilters">
+        <v-text-field
+          hide-details
+          v-model="search"
+          prepend-inner-icon="mdi-magnify"
+          clearable
+          label="Asignatura"
+          type="text"
+          variant="outlined"
+          :append-inner-icon="'mdi-cog-outline'"
+          @click:appendInner="showFilter = !showFilter"
+          @click:clear="clearFilters"
+        >
         </v-text-field>
       </v-container>
       <v-expand-transition v-show="showFilter">
         <v-card-text class="text-subtitle-1">
-          <v-autocomplete v-model="selectedLevel" chips closable-chips multiple clearable :items="availableLevels"
-            label="Seleccione el semestre"></v-autocomplete>
+          <v-autocomplete
+            v-model="selectedLevel"
+            chips
+            closable-chips
+            multiple
+            clearable
+            :items="availableLevels"
+            label="Seleccione el semestre"
+          ></v-autocomplete>
           <v-radio-group inline v-model="selectedShift">
             <v-radio label="Diurno" value="diurno"></v-radio>
-            <v-radio class="mx-auto" label="Vespertino" value="vespertino"></v-radio>
+            <v-radio
+              class="mx-auto"
+              label="Vespertino"
+              value="vespertino"
+            ></v-radio>
           </v-radio-group>
           <v-btn @click="clearFilters">Limpiar Filtros</v-btn>
         </v-card-text>
       </v-expand-transition>
       <v-expansion-panels v-if="groupedSections" variant="accordion">
-        <v-expansion-panel v-for="(group, subjectId) in filteredSubjects" :key="subjectId">
-          <v-expansion-panel-title expand-icon="mdi-menu-down" class="text-body-1">
+        <v-expansion-panel
+          v-for="(group, subjectId) in filteredSubjects"
+          :key="subjectId"
+        >
+          <v-expansion-panel-title
+            expand-icon="mdi-menu-down"
+            class="text-body-1"
+          >
             {{ formatGroupName(group.subject.name) }}
           </v-expansion-panel-title>
           <v-expansion-panel-text>
-            <dm-section-card v-for="section in group.sections" :key="section.id" :section="section"
-              :calendar="calendar" />
+            <dm-section-card
+              v-for="section in group.sections"
+              :key="section.id"
+              :section="section"
+              :calendar="calendar"
+            />
           </v-expansion-panel-text>
         </v-expansion-panel>
       </v-expansion-panels>
@@ -34,8 +64,8 @@
 </template>
 
 <script>
-import { mapActions, mapState } from 'vuex';
-import DmSectionCard from './DmSectionCard.vue';
+import { mapActions, mapState } from "vuex";
+import DmSectionCard from "./DmSectionCard.vue";
 
 export default {
   data: () => ({
@@ -43,22 +73,24 @@ export default {
     show: false,
     showFilter: false,
     selectedLevel: [],
-    selectedShift: ''
+    selectedShift: "",
   }),
 
   components: {
-    DmSectionCard
+    DmSectionCard,
   },
 
   computed: {
-    ...mapState('academicCharges', ['sections']),
+    ...mapState("academicCharges", ["sections"]),
 
     availableLevels() {
-      const levels = this.sections.map((s) => s.subject.level)
+      const levels = this.sections.map((s) => s.subject.level);
 
-      const uniqueLevels = [...new Set(levels)]
+      const uniqueLevels = [...new Set(levels)];
 
-      return uniqueLevels.sort().map((i) => ({ value: i, title: i == 0 ? 'Optativo' : i }));
+      return uniqueLevels
+        .sort()
+        .map((i) => ({ value: i, title: i == 0 ? "Optativo" : i }));
     },
 
     groupedSections() {
@@ -78,14 +110,22 @@ export default {
     },
 
     filteredSubjects() {
-      return Object.values(this.groupedSections).filter(group => {
-        const searchTerm = this.search.toLowerCase().replace(/[-\s]/g, '');
-        const subjectName = group.subject.name.toLowerCase().replace(/[-\s]/g, '');
+      return Object.values(this.groupedSections).filter((group) => {
+        const searchTerm = this.search.toLowerCase().replace(/[-\s]/g, "");
+        const subjectName = group.subject.name
+          .toLowerCase()
+          .replace(/[-\s]/g, "");
         const isMatch = subjectName.includes(searchTerm);
 
-        const hasLevelFilter = this.selectedLevel.length === 0 || this.selectedLevel.includes(group.subject.level);
+        const hasLevelFilter =
+          this.selectedLevel.length === 0 ||
+          this.selectedLevel.includes(group.subject.level);
 
-        const hasShiftFilter = !this.selectedShift || group.sections.some(section => this.selectedShift === section.shift);
+        const hasShiftFilter =
+          !this.selectedShift ||
+          group.sections.some(
+            (section) => this.selectedShift === section.shift
+          );
 
         return isMatch && hasLevelFilter && hasShiftFilter;
       });
@@ -100,26 +140,28 @@ export default {
   },
 
   methods: {
-    ...mapActions('academicCharges', ['getSections']),
+    ...mapActions("academicCharges", ["getSections"]),
     formatGroupName(name) {
-      let sentences = name.replace(/-/g, ' ').split(" ").map(word => {
-        return word[0].toUpperCase() + word.slice(1);
-      })
+      let sentences = name
+        .replace(/-/g, " ")
+        .split(" ")
+        .map((word) => {
+          return word[0].toUpperCase() + word.slice(1);
+        });
       return sentences.join(" ");
     },
 
     clearFilters() {
-      this.search = '';
+      this.search = "";
       this.selectedLevel = [];
       this.selectedShift = null;
     },
-
   },
   async created() {
     await this.getSections({
       academicChargeId: this.calendar.academic_charge.id,
       calendarableId: this.calendar.calendarable.id,
-      calendarableType: this.calendar.calendarable_type.toLowerCase(),
+      calendarableType: this.calendar.calendarable.type.toLowerCase(),
     });
   },
 };
