8e25143ff46ac84eb84cbce1eba1b18092b29598
Rodrigo Pizarro <r.antonio.regueiro@gmail.com>
2023-11-19 17:39:08 -0300

    Refactor DmCalendarPanels to work with an array of calendars

--- src/components/home/DmCalendarPanels.vue
+++ src/components/home/DmCalendarPanels.vue
@@ -1,74 +1,43 @@
 <script setup>
-import { CALENDAR_SOURCES } from "../../helpers/constants";
-import { useStore } from "vuex";
 import { auth } from "../../config/firebase";
-import { computed, onMounted, ref } from "vue";
+import { computed, ref } from "vue";
 import { useRouter } from "vue-router";
 import DmCalendarCard from "../calendar/DmCalendarCard.vue";
 
-const MAX_CALENDARS = 3;
-
-const store = useStore();
 const router = useRouter();
 
-onMounted(async () => {
-  await store.dispatch("calendars/getCalendars");
+const props = defineProps({
+  calendars: {
+    type: Array,
+    required: true,
+  },
+
+  startOpen: {
+    type: Boolean,
+    default: false,
+  },
+
+  title: {
+    type: String,
+    default: "Tus calendarios",
+  },
 });
 
-const localCalendars = computed(() =>
-  store.state.calendars.localCalendars.splice(0, MAX_CALENDARS)
-);
+const email = computed(() => {
+  return auth.currentUser?.email;
+});
 
-const apiCalendars = computed(() =>
-  store.state.calendars.apiCalendars.reverse().splice(0, MAX_CALENDARS)
-);
-
-const email = computed(() => auth.currentUser?.email);
-
-const panels = ref([
-  email.value ? CALENDAR_SOURCES.API : CALENDAR_SOURCES.LOCAL,
-]);
+const panels = ref(props.startOpen ? [0] : []);
 </script>
 
 <template>
-  <v-expansion-panels multiple v-model="panels">
-    <!-- Account calendars -->
-    <v-expansion-panel
-      :value="CALENDAR_SOURCES.API"
-      v-if="email"
-      class="rounded-lg"
-      :class="panels.length > 0 ? 'elevation-4' : ''"
-    >
+  <v-expansion-panels v-model="panels">
+    <v-expansion-panel class="rounded-lg elevation-4">
       <v-expansion-panel-title class="text-h6 font-weight-regular">
-        {{ "Calendarios recientes de " + email }}
+        {{ title }}
       </v-expansion-panel-title>
       <v-expansion-panel-text>
-        <section v-for="calendar in apiCalendars" :key="calendar.uuid">
-          <dm-calendar-card :calendar="calendar" class="my-4">
-          </dm-calendar-card>
-        </section>
-        <section class="text-center">
-          <v-btn size="large" @click="router.push({ name: 'calendars.index' })"
-            >Ver mas calendarios</v-btn
-          >
-        </section>
-      </v-expansion-panel-text>
-    </v-expansion-panel>
-
-    <!-- Device calendars -->
-    <v-expansion-panel
-      :value="CALENDAR_SOURCES.LOCAL"
-      class="rounded-lg"
-      :class="panels.length > 0 ? 'elevation-4' : ''"
-    >
-      <v-expansion-panel-title class="text-h6 font-weight-regular">
-        Ultimos calendarios en este dispositivo
-      </v-expansion-panel-title>
-      <v-expansion-panel-text>
-        <section
-          v-for="(calendar, index) in localCalendars"
-          :key="calendar.uuid"
-        >
+        <section v-for="calendar in calendars" :key="calendar.uuid">
           <dm-calendar-card :calendar="calendar" class="my-4">
           </dm-calendar-card>
         </section>
