b4d4f5b52702ef21d1cb7a19d6bee46c95d0e748
Rodrigo Pizarro <r.antonio.regueiro@gmail.com>
2024-01-16 15:38:54 -0300

    Initial user service to handle user CRUD

---
+++ src/services/UserService.js
@@ -0,0 +1,95 @@
+import { addDoc, collection, updateDoc, doc, getDoc } from "firebase/firestore";
+
+/**
+ * This class is a wrapper to interact with Firebase as a REST API.
+ * It uses Firebase firestore to store user data.
+ *
+ */
+export default class UserService {
+  constructor(firestore, collection) {
+    this.firestore = firestore;
+    this.collection = collection;
+    this.defaultRoles = ["common"];
+  }
+
+  /**
+   * Fetch all users from the database.
+   *
+   * TODO: Add a pagination
+   * TODO: Add filtering and sorting (where, orderBy)
+   *
+   * @returns {array} An array of users.
+   */
+  async index() {
+    throw new Error("Not implemented");
+  }
+
+  /**
+   * Fetch a single user by it id.
+   *
+   * @param {string} id
+   * @returns {object | null} The user object or null if not found.
+   *
+   * @see https://firebase.google.com/docs/firestore/query-data/get-data
+   */
+  async show(id) {
+    const docRef = doc(this.firestore, this.collection, id);
+    const docSnap = await getDoc(docRef);
+
+    if (!docSnap.exists()) return null;
+
+    return {
+      id: docSnap.id,
+      ...docSnap.data(),
+    };
+  }
+
+  /**
+   * Send a new user into the database and return the created user.
+   *
+   * @param {object} user The user provided via Firebase Auth.
+   * @returns {object} The created user
+   *
+   * @see https://firebase.google.com/docs/firestore/manage-data/add-data
+   * @see https://firebase.google.com/docs/reference/js/v8/firebase.User
+   */
+  async store(data) {
+    // Create an user using the given id inside data
+    const { uid, email, displayName } = data;
+    const userRef = doc(this.firestore, this.collection, uid);
+
+    // Add user
+    const response = await addDoc(userRef, {
+      email,
+      displayName,
+      roles: this.defaultRoles, // Roles are always added by default as a common user
+    });
+
+    // Fetch user from firestore
+    const userSnap = await getDoc(response);
+
+    return {
+      id: userSnap.id,
+      ...userSnap.data(),
+    };
+  }
+
+  /**
+   * Update an user by its id.
+   * @param {string} id
+   * @param {object} data An object representation of the resource
+   */
+  async update(id, data) {
+    const docRef = doc(this.firestore, this.collection, id);
+    const userRef = await updateDoc(docRef, data);
+
+    return {
+      id: userRef.id,
+      ...userRef.data(),
+    };
+  }
+
+  async destroy(id) {
+    throw new Error("Not implemented");
+  }
+}
