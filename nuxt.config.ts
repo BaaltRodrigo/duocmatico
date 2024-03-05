524d0fbc8211852e57a43567676b8cdb34fd5de6
BaaltRodrigo <r.antonio.regueiro@gmail.com>
2024-03-04 23:50:29 -0300

    Nuxt initial steps

---
+++ nuxt.config.ts
@@ -0,0 +1,27 @@
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
+
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+// @see https://vuetifyjs.com/en/getting-started/installation/#using-nuxt-3
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  build: {
+    transpile: ['vuetify'],
+  },
+  modules: [
+    (_options, nuxt) => {
+      nuxt.hooks.hook('vite:extendConfig', (config) => {
+        // @ts-expect-error
+        config.plugins.push(vuetify({ autoImport: true }))
+      })
+    },
+    //...
+  ],
+  vite: {
+    vue: {
+      template: {
+        transformAssetUrls,
+      },
+    },
+  },
+})
