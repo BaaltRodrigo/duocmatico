b68457774d4727647960f6f2c3f907eb79cb6e41
Rodrigo Pizarro <r.antonio.regueiro@gmail.com>
2024-03-10 23:26:21 -0300

    ✨ Hero component

---
+++ components/home/hero.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="text-center py-6">
+    <h1 class="text-h4 text-white font-weight-regular text-wrap-balance">
+      Te ayudamos a planifificar mejor tu proximo semestre sin importar la sede
+      en la que te encuentres
+    </h1>
+    <v-btn
+      class="rounded-lg text-none mt-6"
+      size="x-large"
+      color="white"
+      @click="$router.push({ name: 'login' })"
+      >Empieza ahora</v-btn
+    >
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { useDisplay } from "vuetify";
+
+const { mobile } = useDisplay();
+</script>
+
+<style scoped>
+.text-wrap-balance {
+  text-wrap: balance;
+}
+</style>
