ccb6deca4fe60b0fb9c67401118c4c2ebeb3bdb6
Rodrigo Pizarro <r.antonio.regueiro@gmail.com>
2024-03-16 00:11:08 -0300

    ✨ hero and feature cards for home

---
+++ components/home/featureCard.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-card class="rounded-xl bg-white" variant="outlined" height="100%">
+    <v-card-text>
+      <v-card height="200" variant="tonal" color="grey" class="rounded-xl pa-2">
+        <v-img :src="feature.image" :aspect-ration="9 / 16"></v-img>
+      </v-card>
+      <v-card-title class="px-1"> {{ feature.title }} </v-card-title>
+      <v-list-item v-for="item in feature.items" :key="item.title" class="mt-2">
+        <template #prepend>
+          <v-avatar color="purple" size="small">
+            <v-icon> {{ item.icon }} </v-icon>
+          </v-avatar>
+        </template>
+        {{ item.title }}
+      </v-list-item>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script lang="ts" setup>
+defineProps({
+  feature: {
+    type: Object,
+    required: true,
+  },
+});
+</script>
--- components/home/hero.vue
+++ components/home/hero.vue
@@ -1,23 +1,22 @@
 <template>
   <div class="text-center py-6">
     <h1 class="text-h4 text-white font-weight-regular text-wrap-balance">
-      Te ayudamos a planifificar mejor tu proximo semestre sin importar la sede
-      en la que te encuentres
+      Te ayudamos a planificar mejor tu proximo semestre sin importar la sede en
+      la que te encuentres
     </h1>
     <v-btn
       class="rounded-lg text-none mt-6"
       size="x-large"
       color="white"
       @click="$router.push({ name: 'login' })"
-      >Empieza ahora</v-btn
     >
+      Empieza ahora
+    </v-btn>
   </div>
 </template>
 
 <script lang="ts" setup>
 import { useDisplay } from "vuetify";
-
-const { mobile } = useDisplay();
 </script>
 
 <style scoped>
